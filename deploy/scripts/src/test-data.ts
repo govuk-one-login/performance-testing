6ebc9d23d26c3ac67d0d3bacdcd286b2fb7e90a2
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2022-11-23 13:41:32 +0000

    Add test data example

---
+++ deploy/scripts/src/test-data.ts
@@ -0,0 +1,52 @@
+import { Options } from 'k6/options';
+import { SharedArray } from 'k6/data';
+import exec from 'k6/execution';
+
+export const options: Options = {
+  scenarios: {
+    csv : {
+      executor: 'shared-iterations',
+      vus: 3,
+      iterations: 8,
+      exec: 'csv'
+    },
+    json: {
+      executor: 'shared-iterations',
+      vus: 3,
+      iterations: 8,
+      exec: 'json'
+    }
+  },
+  thresholds: {
+    http_req_duration: ['p(95)<500'],
+  },
+};
+
+type User = {username: string, pass: string}
+// Parse JSON data
+// From https://k6.io/docs/examples/data-parameterization/#from-a-json-file
+const jsonData: User[] = new SharedArray('json', function () {
+  return JSON.parse(open('./data/example.json')).users;
+});
+// Parse CSV data
+// Also https://k6.io/docs/examples/data-parameterization/#from-a-csv-file
+const csvData: User[] = new SharedArray('csv', function () {
+  return open('./data/example.csv').split('\n').slice(1).map(s => {
+    let data = s.split(',');
+    return {username: data[0], pass: data[1]}
+  });
+});
+
+export function setup() {
+  console.log('VU | Scenario | Username | Password')
+}
+
+export function csv() {
+  const user= csvData[exec.scenario.iterationInTest % jsonData.length];
+  console.log(exec.vu.idInTest, ' | csv      |', user.username, '   |', user.pass);
+}
+
+export function json() {
+  const user= jsonData[exec.scenario.iterationInTest % jsonData.length];
+  console.log(exec.vu.idInTest, ' | json     |', user.username, '   |', user.pass);
+}

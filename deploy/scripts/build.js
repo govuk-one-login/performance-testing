52cf1067ca4444426f98ceaedbbc42e4c695287e
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2023-01-25 16:38:14 +0000

    Lint build.js

--- deploy/scripts/build.js
+++ deploy/scripts/build.js
@@ -1,34 +1,33 @@
-const { build } = require("esbuild");
-const { copy } =  require("esbuild-plugin-copy");
-const glob = require('glob');
-const outbase = './src';
-const outdir = './dist';
+const { build } = require('esbuild')
+const { copy } = require('esbuild-plugin-copy')
+const glob = require('glob')
+const outbase = './src'
+const outdir = './dist'
 
 build({
-    entryPoints: glob.sync('src/*.ts'),
-    outbase,
-    outdir,
-    target: 'es6',
-    format: 'esm',
-    bundle: true,
-    sourcemap: true,
-    watch: false,
-    minify: false,
-    external: ["k6*", "https://*"],
-    plugins: [
-        copy({
-          assets: {
-            from: [outbase + '/data/*'],
-            to: ['./data'],
-          },
-        }),
-      ],
+  entryPoints: glob.sync('src/*.ts'),
+  outbase,
+  outdir,
+  target: 'es6',
+  format: 'esm',
+  bundle: true,
+  sourcemap: true,
+  watch: false,
+  minify: false,
+  external: ['k6*', 'https://*'],
+  plugins: [
+    copy({
+      assets: {
+        from: [outbase + '/data/*'],
+        to: ['./data']
+      }
+    })
+  ]
 })
-.then(() => {
-    console.log("Transpiled files generated:");
+  .then(() => {
+    console.log('Transpiled files generated:')
     glob.sync(outdir + '/*').forEach(file => {
-        console.log(`+ \x1b[32m${file}\x1b[0m`)
-    });
-})
-.catch(() => process.exit(1));
-
+      console.log(`+ \x1b[32m${file}\x1b[0m`)
+    })
+  })
+  .catch(() => process.exit(1))

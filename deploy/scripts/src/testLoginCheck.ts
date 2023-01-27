e92beb8ef6b73ef104958d19f9e35cb5f06544a6
Abhinay Srivastava <117734822+abhinaysrivastavaperf@users.noreply.github.com>
2023-01-27 11:42:46 +0000

    QA-85-1 (#22)
    * Add eslint ts standard package\
    
    Adding eslint-config-standard-with-typescript \
    package to enforce StandardJS linting rules
    
    * Lint devplatform-test.ts
    
    * Lint test-data.ts
    
    * Lint test.ts
    
    * Lint unit-tests.ts
    
    * Lint totp.ts
    
    * Lint load-profiles.ts
    
    * Lint build.js
    
    * Lint test-accountManagement.ts
    
    * Lint testLoginCheck.ts
    
    Co-authored-by: Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>

--- deploy/scripts/src/testLoginCheck.ts
+++ deploy/scripts/src/testLoginCheck.ts
@@ -1,16 +1,16 @@
-import TOTP from './utils/authentication/totp';
+import TOTP from './utils/authentication/totp'
 
-/* This script generates the TOTP when you want to login manually (on your local machine into Accounts test environment using the test users created by the bulk test user creation. 
+/* This script generates the TOTP when you want to login manually (on your local machine into Accounts test environment using the test users created by the bulk test user creation.
 As Auth App Key is a secret, the value has been left blank in the repo. Please follow the below steps.
 
 1.) Update the value of the Auth App Key used for bulk users in the below code.
 2.) Run the script. This will print the TOTP on the console. Use the TOTP to login manually.
 */
 
-export default function(){
-    let secretKey =''; // Left blank - DO NOT COMMIT WITH THE VALUE OF THE AUTH APP KEY HERE
-    let totp = new TOTP(secretKey);
-    let code = totp.generateTOTP();
+export default function (): void {
+  const secretKey = '' // Left blank - DO NOT COMMIT WITH THE VALUE OF THE AUTH APP KEY HERE
+  const totp = new TOTP(secretKey)
+  const code = totp.generateTOTP()
 
-    console.log(code);
+  console.log(code)
 }

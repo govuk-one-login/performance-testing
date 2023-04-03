18d117048b480c6c762f7523c5bd99caea1fe148
George <gpopescu@deloittece.com>
2023-04-03 13:21:58 +0300

    Updated threshold with 99th percentile NFR

--- deploy/scripts/src/test-LimeCRI.ts
+++ deploy/scripts/src/test-LimeCRI.ts
@@ -88,7 +88,7 @@ const loadProfile = selectProfile(profiles)
 export const options: Options = {
   scenarios: loadProfile.scenarios,
   thresholds: {
-    http_req_duration: ['p(95)<1000'], // 95th percntile response time <1000ms
+    http_req_duration: ['p(95)<=1000', 'p(99)<=2500'], // 95th percentile response time <=1000ms, 99th percentile response time <=2500ms
     http_req_failed: ['rate<0.05'] // Error rate <5%
   }
 }

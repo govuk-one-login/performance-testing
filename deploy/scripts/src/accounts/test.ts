b7cbf50b3ad305fb52aa990a5e2c5625b31fc5af
Abhinay Srivastava <abhinay.srivastava@digital.cabinet-office.gov.uk>
2023-06-09 14:04:35 +0100

    Updated workload model for Account Management scenarios

--- deploy/scripts/src/accounts/test.ts
+++ deploy/scripts/src/accounts/test.ts
@@ -63,10 +63,10 @@ const profiles: ProfileList = {
       startRate: 1,
       timeUnit: '1s',
       preAllocatedVUs: 1,
-      maxVUs: 300,
+      maxVUs: 1000,
       stages: [
-        { target: 10, duration: '30m' }, // Ramp up to 30 iterations per second in 30 minutes
-        { target: 10, duration: '15m' }, // Steady State of 15 minutes at the ramp up load i.e. 30 iterations/second
+        { target: 10, duration: '15m' }, // Ramp up to 10 iterations per second in 15 minutes
+        { target: 10, duration: '30m' }, // Steady State of 30 minutes at the ramp up load i.e. 10 iterations/second
         { target: 0, duration: '5m' } // Ramp down duration of 5 minutes.
       ],
       exec: 'changeEmail'
@@ -77,10 +77,10 @@ const profiles: ProfileList = {
       startRate: 1,
       timeUnit: '1s',
       preAllocatedVUs: 1,
-      maxVUs: 300,
+      maxVUs: 1000,
       stages: [
-        { target: 10, duration: '30m' }, // Ramp up to 30 iterations per second in 30 minutes
-        { target: 10, duration: '15m' }, // Steady State of 15 minutes at the ramp up load i.e. 30 iterations/second
+        { target: 10, duration: '15m' }, // Ramp up to 10 iterations per second in 15 minutes
+        { target: 10, duration: '30m' }, // Steady State of 30 minutes at the ramp up load i.e. 10 iterations/second
         { target: 0, duration: '5m' } // Ramp down duration of 5 minutes.
       ],
       exec: 'changePassword'
@@ -91,10 +91,10 @@ const profiles: ProfileList = {
       startRate: 1,
       timeUnit: '1s',
       preAllocatedVUs: 1,
-      maxVUs: 300,
+      maxVUs: 1000,
       stages: [
-        { target: 10, duration: '30m' }, // Ramp up to 30 iterations per second in 30 minutes
-        { target: 10, duration: '15m' }, // Steady State of 15 minutes at the ramp up load i.e. 30 iterations/second
+        { target: 10, duration: '15m' }, // Ramp up to 10 iterations per second in 15 minutes
+        { target: 10, duration: '30m' }, // Steady State of 30 minutes at the ramp up load i.e. 10 iterations/second
         { target: 0, duration: '5m' } // Ramp down duration of 5 minutes.
       ],
       exec: 'changePhone'
@@ -105,10 +105,10 @@ const profiles: ProfileList = {
       startRate: 1,
       timeUnit: '1s',
       preAllocatedVUs: 1,
-      maxVUs: 250,
+      maxVUs: 1000,
       stages: [
-        { target: 10, duration: '30m' }, // Ramp up to 30 iterations per second in 30 minutes
-        { target: 10, duration: '15m' }, // Steady State of 15 minutes at the ramp up load i.e. 30 iterations/second
+        { target: 10, duration: '15m' }, // Ramp up to 10 iterations per second in 15 minutes
+        { target: 10, duration: '30m' }, // Steady State of 30 minutes at the ramp up load i.e. 10 iterations/second
         { target: 0, duration: '5m' } // Ramp down duration of 5 minutes.
       ],
       exec: 'deleteAccount'

3de6edcfe0ddab55e9772b73b13eb183e2043c99
Jamie Lutton <jamie.lutton@digital.cabinet-office.gov.uk>
2023-06-28 08:33:06 +0100

    DCMAW-5594: Add comment explaining __ENV

--- deploy/scripts/src/mobile/utils/config.ts
+++ deploy/scripts/src/mobile/utils/config.ts
@@ -1,3 +1,5 @@
+// __ENV.<variable> refers to value of environment variable, either passed via the CLI
+// or defined in the env/parameter-store section of the template.yaml
 export const config = {
   testClientExecuteUrl: __ENV.MOBILE_TEST_CLIENT_EXECUTE_URL,
   backendUrl: __ENV.MOBILE_BACKEND_URL,

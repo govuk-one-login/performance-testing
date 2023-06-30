2c1aeab02f2aea0c31d60156bc2b8ad7504716ed
Paul Flood <114575593+paulfloodgds@users.noreply.github.com>
2023-06-30 16:11:54 +0100

    [QA-209] Add scripts for BTM and TxMA (#162)
    * Create TXMA and BTM Folders
    
    * Add create-load-profile.ts
    
    * update create-load-profile.ts with env variables
    
    * Update TXMA test.ts - add imports
    
    * Update TXMA and BTM test.ts locally verified
    
    * removed ; from UUID import
    
    * Removed  TXMA utils folders - not need for test.ts
    
    * added sqs.ts and index.js
    
    * added BTM_AWS_SQS_QUEUE  to template.yaml
    
    * added k6-utils to common utils
    
    * add k6 and AWS to utils
    
    * udpate template.yaml
    
    * update template.yaml
    
    * update template.yaml
    
    * udpate template.yaml
    
    * fix eslint errors
    
    * fix lint
    
    * Update Env Prefix to Data.TXMA DATA.BTM
    
    ---------
    
    Co-authored-by: Tom Dann <110121463+Tom-Dann@users.noreply.github.com>

---
+++ deploy/scripts/src/txma/test.ts
@@ -0,0 +1,58 @@
+import { type Options } from 'k6/options'
+import { selectProfile, type ProfileList, describeProfile } from '../common/utils/config/load-profiles'
+import { uuidv4 } from '../common/utils/k6/k6-utils'
+import { AWSConfig, SQSClient } from '../common/utils/aws/sqs'
+const profiles: ProfileList = {
+  smoke: {
+    sendEventSmokeTest1: {
+      executor: 'ramping-arrival-rate',
+      startRate: 1,
+      timeUnit: '1s',
+      preAllocatedVUs: 1,
+      maxVUs: 2,
+      stages: [
+        { target: 1, duration: '5s' } // Ramps up to target load
+      ],
+      exec: 'sendEvent'
+    }
+  }
+}
+
+const loadProfile = selectProfile(profiles)
+
+export const options: Options = {
+  scenarios: loadProfile.scenarios,
+  thresholds: {
+    http_req_duration: ['p(95)<=1000', 'p(99)<=2500'], // 95th percentile response time <=1000ms, 99th percentile response time <=2500ms
+    http_req_failed: ['rate<0.05'] // Error rate <5%
+  }
+}
+
+export function setup (): void {
+  describeProfile(loadProfile)
+}
+
+const env = {
+  sqs_queue: __ENV.DATA_TXMA_SQS
+}
+
+const awsConfig = new AWSConfig({
+  region: __ENV.DATA_TXMA_AWS_REGION,
+  accessKeyId: __ENV.DATA_TXMA_AWS_ACCESS_KEY_ID,
+  secretAccessKey: __ENV.DATA_TXMA_AWS_SECRET_ACCESS_KEY,
+  sessionToken: __ENV.DATA_TXMA_AWS_SESSION_TOKEN
+})
+
+const eventData = {
+  payload: __ENV.DATA_TXMA_SQS_PAYLOAD
+}
+
+const sqs = new SQSClient(awsConfig)
+
+export function sendEvent (): void {
+  const messageBody = eventData.payload.replace(/UUID/g, () => uuidv4())
+
+  sqs.sendMessage(env.sqs_queue, messageBody)
+
+  console.log('3 === debug === messageBody', messageBody)
+}

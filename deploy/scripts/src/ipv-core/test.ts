b0eb6a596237926382d7ba76a5e00a3fb3b421f9
Abhinay Srivastava <abhinay.srivastava@digital.cabinet-office.gov.uk>
2023-06-30 13:24:54 +0100

    Updated IPV Core script

--- deploy/scripts/src/ipv-core/test.ts
+++ deploy/scripts/src/ipv-core/test.ts
@@ -91,7 +91,7 @@ export function coreScenario1 (): void {
   let res: Response
   let csrfToken: string
   let resourceID: string
-  let uniqueUserID: string
+  let dcmawStubURL: string
   let passportStubURL: string
   let addressStubURL: string
   let fraudStubURL: string
@@ -111,25 +111,23 @@ export function coreScenario1 (): void {
 
       check(res, {
         'is status 200': (r) => r.status === 200,
-        'verify page content': (r) => (r.body as string).includes('Choose a user id value')
+        'verify page content': (r) => (r.body as string).includes('Enter userId manually')
       })
         ? transactionDuration.add(endTime - startTime)
         : fail('Response Validation Failed')
-
-      uniqueUserID = res.html().find('select[name=userIdSelect]>option').last().val() ?? fail('User UUID not found')
     }
   )
 
   sleep(Math.random() * 3)
 
   group(
-    'B01_Core_02_SelectUserIDContinue GET',
+    'B01_Core_02_GoToFullJourneyRoute GET',
     function () {
       const startTime = Date.now()
       res = http.get(
-        env.orchStubEndPoint + `/authorize?journeyType=full&userIdSelect=${uniqueUserID}&userIdText=`,
+        env.orchStubEndPoint + '/authorize?journeyType=full&userIdText=',
         {
-          tags: { name: 'B01_Core_02_SelectUserIDContinue' }
+          tags: { name: 'B01_Core_02_GoToFullJourneyRoute' }
         }
       )
       const endTime = Date.now()
@@ -148,25 +146,99 @@ export function coreScenario1 (): void {
   sleep(Math.random() * 3)
 
   group(
-    'B01_Core_03_ClickContinueAfterLogin POST',
+    'B01_Core_03_ClickStartAfterLogin POST',
     function () {
-      const startTime = Date.now()
+      const startTime1 = Date.now()
       res = http.post(
         env.ipvCoreURL + '/ipv/page/page-ipv-identity-start',
         {
           _csrf: csrfToken
         },
         {
-          tags: { name: 'B01_Core_03_ClickContinueAfterLogin' }
+          redirects: 0,
+          tags: { name: 'B01_Core_03_ClickStartAfterLogin_01_Core' }
         }
       )
-      const endTime = Date.now()
+      const endTime1 = Date.now()
+
+      check(res, {
+        'is status 302': (r) => r.status === 302
+      })
+        ? transactionDuration.add(endTime1 - startTime1)
+        : fail('Response Validation Failed')
+
+      dcmawStubURL = res.headers.Location
+
+      const startTime2 = Date.now()
+      res = http.get(res.headers.Location,
+        {
+          tags: { name: 'B01_Core_03_ClickStartAfterLogin_02_DcmawStub' }
+        }
+      )
+      const endTime2 = Date.now()
 
       check(res, {
         'is status 200': (r) => r.status === 200,
-        'verify page content': (r) => (r.body as string).includes('Enter the details from your photo ID and answer security questions')
+        'verify page content': (r) => (r.body as string).includes('DOC Checking App (Stub)')
       })
-        ? transactionDuration.add(endTime - startTime)
+        ? transactionDuration.add(endTime2 - startTime2)
+        : fail('Response Validation Failed')
+
+      resourceID = getResourceID(res)
+    }
+  )
+
+  sleep(Math.random() * 3)
+
+  group(
+    'B01_Core_04_DCMAWContinue POST',
+    function () {
+      const startTime1 = Date.now()
+      res = http.post(
+        dcmawStubURL,
+        {
+          jsonPayload: '',
+          strengthScore: '',
+          validityScore: '',
+          activityHistoryScore: '',
+          biometricVerificationScore: '',
+          ci: '',
+          evidenceJsonPayload: '',
+          expHours: '0',
+          expMinutes: '0',
+          expSeconds: '0',
+          requested_oauth_error_endpoint: 'auth',
+          requested_oauth_error: 'access_denied',
+          requested_oauth_error_description: 'This error was triggered manually in the stub CRI',
+          resourceId: resourceID,
+          submit: 'Submit data and generate auth code'
+        },
+        {
+          redirects: 1,
+          tags: { name: 'B01_Core_04_DCMAWContinue_1_DcmawStub' }
+        }
+      )
+      const endTime1 = Date.now()
+
+      check(res, {
+        'is status 302': (r) => r.status === 302
+      })
+        ? transactionDuration.add(endTime1 - startTime1)
+        : fail('Response Validation Failed')
+
+      const startTime2 = Date.now()
+      res = http.get(env.ipvCoreURL + res.headers.Location,
+        {
+          tags: { name: 'B01_Core_04_DCMAWContinue_2_Core' }
+        }
+      )
+      const endTime2 = Date.now()
+
+      check(res, {
+        'is status 200': (r) => r.status === 200,
+        'verify page content': (r) => (r.body as string).includes('Which photo ID would you like to use')
+      })
+        ? transactionDuration.add(endTime2 - startTime2)
         : fail('Response Validation Failed')
 
       csrfToken = getCSRF(res)
@@ -176,7 +248,7 @@ export function coreScenario1 (): void {
   sleep(Math.random() * 3)
 
   group(
-    'B01_Core_04_ContinueOnPYIStartPage POST',
+    'B01_Core_05_ContinueOnPYIStartPage POST',
     function () {
       const startTime1 = Date.now()
       res = http.post(
@@ -187,7 +259,7 @@ export function coreScenario1 (): void {
         },
         {
           redirects: 0,
-          tags: { name: 'B01_Core_04_ContinueOnPYIStartPage_01_Core' }
+          tags: { name: 'B01_Core_05_ContinueOnPYIStartPage_01_Core' }
         }
       )
       const endTime1 = Date.now()
@@ -203,7 +275,7 @@ export function coreScenario1 (): void {
       const startTime2 = Date.now()
       res = http.get(res.headers.Location,
         {
-          tags: { name: 'B01_Core_04_ContinueOnPYIStartPage_02_PassStub' }
+          tags: { name: 'B01_Core_05_ContinueOnPYIStartPage_02_PassStub' }
         }
       )
       const endTime2 = Date.now()
@@ -222,12 +294,13 @@ export function coreScenario1 (): void {
   sleep(Math.random() * 3)
 
   group(
-    'B01_Core_05_PassportDataContinue POST',
+    'B01_Core_06_PassportDataContinue POST',
     function () {
       const startTime1 = Date.now()
       res = http.post(
         passportStubURL,
         {
+          sort: 'Kenneth Decerqueira (Valid Experian) Passport',
           jsonPayload: passportData,
           strengthScore: '4',
           validityScore: '2',
@@ -244,7 +317,7 @@ export function coreScenario1 (): void {
         },
         {
           redirects: 0,
-          tags: { name: 'B01_Core_05_PassportDataContinue_1_PassStub' }
+          tags: { name: 'B01_Core_06_PassportDataContinue_1_PassStub' }
         }
       )
       const endTime1 = Date.now()
@@ -259,7 +332,7 @@ export function coreScenario1 (): void {
       res = http.get(res.headers.Location,
         {
           redirects: 0,
-          tags: { name: 'B01_Core_05_PassportDataContinue_2_Core' }
+          tags: { name: 'B01_Core_06_PassportDataContinue_2_Core' }
         }
       )
       const endTime2 = Date.now()
@@ -275,7 +348,7 @@ export function coreScenario1 (): void {
       const startTime3 = Date.now()
       res = http.get(res.headers.Location,
         {
-          tags: { name: 'B01_Core_05_PassportDataContinue_3_AddStub' }
+          tags: { name: 'B01_Core_06_PassportDataContinue_3_AddStub' }
         }
       )
       const endTime3 = Date.now()
@@ -294,12 +367,13 @@ export function coreScenario1 (): void {
   sleep(Math.random() * 3)
 
   group(
-    'B01_Core_06_AddrDataContinue POST',
+    'B01_Core_07_AddrDataContinue POST',
     function () {
       const startTime1 = Date.now()
       res = http.post(
         addressStubURL,
         {
+          sort: 'Kenneth Decerqueira (Valid Experian) Address',
           jsonPayload: addressData,
           expHours: '0',
           expMinutes: '0',
@@ -313,7 +387,7 @@ export function coreScenario1 (): void {
         },
         {
           redirects: 0,
-          tags: { name: 'B01_Core_06_AddrDataContinue_1_AddStub' }
+          tags: { name: 'B01_Core_07_AddrDataContinue_1_AddStub' }
         }
       )
       const endTime1 = Date.now()
@@ -328,7 +402,7 @@ export function coreScenario1 (): void {
       res = http.get(res.headers.Location,
         {
           redirects: 0,
-          tags: { name: 'B01_Core_06_AddrDataContinue_2_Core' }
+          tags: { name: 'B01_Core_07_AddrDataContinue_2_Core' }
         }
       )
       const endTime2 = Date.now()
@@ -344,7 +418,7 @@ export function coreScenario1 (): void {
       const startTime3 = Date.now()
       res = http.get(res.headers.Location,
         {
-          tags: { name: 'B01_Core_06_AddrDataContinue_3_FraudStub' }
+          tags: { name: 'B01_Core_07_AddrDataContinue_3_FraudStub' }
         }
       )
       const endTime3 = Date.now()
@@ -363,14 +437,16 @@ export function coreScenario1 (): void {
   sleep(Math.random() * 3)
 
   group(
-    'B01_Core_07_FraudDataContinue POST',
+    'B01_Core_08_FraudDataContinue POST',
     function () {
       const startTime1 = Date.now()
       res = http.post(
         fraudStubURL,
         {
+          sort: 'Kenneth Decerqueira (Valid Experian) Fraud',
           jsonPayload: fraudData,
           identityFraudScore: '2',
+          activityHistoryScore: '',
           evidenceJsonPayload: '',
           expHours: '0',
           expMinutes: '0',
@@ -384,7 +460,7 @@ export function coreScenario1 (): void {
         },
         {
           redirects: 0,
-          tags: { name: 'B01_Core_07_FraudDataContinue_1_FraudStub' }
+          tags: { name: 'B01_Core_08_FraudDataContinue_1_FraudStub' }
         }
       )
       const endTime1 = Date.now()
@@ -398,7 +474,7 @@ export function coreScenario1 (): void {
       const startTime2 = Date.now()
       res = http.get(res.headers.Location,
         {
-          tags: { name: 'B01_Core_07_FraudDataContinue_2_Core' }
+          tags: { name: 'B01_Core_08_FraudDataContinue_2_Core' }
         }
       )
       const endTime2 = Date.now()
@@ -417,7 +493,7 @@ export function coreScenario1 (): void {
   sleep(Math.random() * 3)
 
   group(
-    'B01_Core_08_PreKBVTransition POST',
+    'B01_Core_09_PreKBVTransition POST',
     function () {
       const startTime1 = Date.now()
       res = http.post(
@@ -427,7 +503,7 @@ export function coreScenario1 (): void {
         },
         {
           redirects: 0,
-          tags: { name: 'B01_Core_08_PreKBVTransition_1_Core' }
+          tags: { name: 'B01_Core_09_PreKBVTransition_1_Core' }
         }
       )
       const endTime1 = Date.now()
@@ -443,7 +519,7 @@ export function coreScenario1 (): void {
       const startTime2 = Date.now()
       res = http.get(res.headers.Location,
         {
-          tags: { name: 'B01_Core_08_PreKBVTransition_2_KBVStub' }
+          tags: { name: 'B01_Core_09_PreKBVTransition_2_KBVStub' }
         }
       )
       const endTime2 = Date.now()
@@ -462,12 +538,13 @@ export function coreScenario1 (): void {
   sleep(Math.random() * 3)
 
   group(
-    'B01_Core_09_KBVDataContinue POST',
+    'B01_Core_10_KBVDataContinue POST',
     function () {
       const startTime1 = Date.now()
       res = http.post(
         kbvStubURL,
         {
+          sort: 'Kenneth Decerqueira (Valid Experian) KBV',
           jsonPayload: kbvData,
           verificationScore: '2',
           evidenceJsonPayload: '',
@@ -483,7 +560,7 @@ export function coreScenario1 (): void {
         },
         {
           redirects: 0,
-          tags: { name: 'B01_Core_09_KBVDataContinue_1_KBVStub' }
+          tags: { name: 'B01_Core_10_KBVDataContinue_1_KBVStub' }
         }
       )
       const endTime1 = Date.now()
@@ -497,7 +574,7 @@ export function coreScenario1 (): void {
       const startTime2 = Date.now()
       res = http.get(res.headers.Location,
         {
-          tags: { name: 'B01_Core_09_KBVDataContinue_2_Core' }
+          tags: { name: 'B01_Core_10_KBVDataContinue_2_Core' }
         }
       )
       const endTime2 = Date.now()
@@ -516,7 +593,7 @@ export function coreScenario1 (): void {
   sleep(Math.random() * 3)
 
   group(
-    'B01_Core_10_ContinuePYISuccessPage POST',
+    'B01_Core_11_ContinuePYISuccessPage POST',
     function () {
       const startTime1 = Date.now()
       res = http.post(
@@ -526,7 +603,7 @@ export function coreScenario1 (): void {
         },
         {
           redirects: 0,
-          tags: { name: 'B01_Core_10_ContinuePYISuccessPage_1_Core' }
+          tags: { name: 'B01_Core_11_ContinuePYISuccessPage_1_Core' }
         }
       )
       const endTime1 = Date.now()
@@ -540,7 +617,7 @@ export function coreScenario1 (): void {
       const startTime2 = Date.now()
       res = http.get(res.headers.Location,
         {
-          tags: { name: 'B01_Core_10_ContinuePYISuccessPage_2_OrchStub' }
+          tags: { name: 'B01_Core_11_ContinuePYISuccessPage_2_OrchStub' }
         }
       )
       const endTime2 = Date.now()

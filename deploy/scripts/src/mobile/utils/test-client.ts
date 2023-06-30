a5bf2dcd82de8adee2eb91bd02a74f0a4431a958
Raluca Alecu <raluca-elena.alecu@digital.cabinet-office.gov.uk>
2023-06-30 11:15:56 +0300

    DCMAW-5611: added parseApiLocation function

--- deploy/scripts/src/mobile/utils/test-client.ts
+++ deploy/scripts/src/mobile/utils/test-client.ts
@@ -1,11 +1,14 @@
 import http, { type Response } from 'k6/http'
-import { buildUrl } from './url'
+import { buildBackendUrl, buildUrl } from './url'
 import { config } from './config'
 
 export function postTestClientStart (): Response {
   return http.post(
     buildUrl('start', config.testClientExecuteUrl),
-    JSON.stringify({ target: config.backendUrl, frontendUri: config.frontendUrl }),
+    JSON.stringify({
+      target: config.backendUrl,
+      frontendUri: config.frontendUrl
+    }),
     {
       tags: { name: 'Post request to authorize URL' },
       headers: { 'Content-Type': 'application/json' }
@@ -13,11 +16,39 @@ export function postTestClientStart (): Response {
   )
 }
 
-export function parseTestClientResponse (response: Response, location: 'WebLocation' | 'ApiLocation'): string {
-  const url = response.json(location)
+export function parseTestClientResponse (
+  response: Response,
+  location: 'WebLocation' | 'ApiLocation'
+): string {
+  let url
+  if (location === 'ApiLocation') {
+    const apiLocation = response.json(location) as string
+    url = parseApiLocation(apiLocation)
+  } else {
+    url = response.json(location)
+  }
+
   if (typeof url !== 'string') {
     throw new Error('Failed to parse URL from response')
   }
 
-  return url.replace(config.backendUrl, `${config.backendUrl}/`)
+  return url
+}
+
+export function parseApiLocation (apiLocation: string): string {
+  const queryParams = apiLocation.split('?')[1].split('&')
+  const requestJwt = queryParams
+    .filter((value) => value.startsWith('request'))[0]
+    .split('=')[1]
+  const clientId = queryParams
+    .filter((value) => value.startsWith('client_id'))[0]
+    .split('=')[1]
+
+  const apiLocationUrl = buildBackendUrl('verifyAuthorizeRequest', {
+    client_id: clientId,
+    response_type: 'code',
+    request: requestJwt
+  })
+
+  return apiLocationUrl
 }

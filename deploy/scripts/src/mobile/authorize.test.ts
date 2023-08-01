a6580446f6a5f59a4766564621dcb588c4279751
Raluca Alecu <raluca-elena.alecu@digital.cabinet-office.gov.uk>
2023-08-01 17:42:01 +0300

    DCMAW-5597: changed maxVUs and startRate

--- deploy/scripts/src/mobile/authorize.test.ts
+++ deploy/scripts/src/mobile/authorize.test.ts
@@ -26,10 +26,10 @@ const profiles: ProfileList = {
   load: {
     startJourney: {
       executor: 'ramping-arrival-rate',
-      startRate: 0,
+      startRate: 1,
       timeUnit: '1s',
       preAllocatedVUs: 50, // Calculation: 100 journeys / second * 0.5 seconds average journey time
-      maxVUs: 200, // Calculation: 100 journeys / second * 2 seconds maximum journey time
+      maxVUs: 300, // Calculation: 100 journeys / second * 2.5 seconds maximum expected from NFR
       stages: [
         { target: 100, duration: '15m' }, // linear increase from 0 iteration per second to 100 iterations per second for 15 min -> 0.11 t/s/s
         { target: 100, duration: '30m' } // maintain 100 iterations per second for 30 min

8021c8f1f83e193ab0cf719fa915ba2fc1c056c8
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2023-08-09 16:22:29 +0100

    Use `submitForm()` where applicable in address.ts

--- deploy/scripts/src/cri-orange/address.ts
+++ deploy/scripts/src/cri-orange/address.ts
@@ -84,9 +84,7 @@ const transactionDuration = new Trend('duration', true)
 
 export function addressScenario1 (): void {
   let res: Response
-  let csrfToken: string
   let fullAddress: string
-  let addDetails: any
   const user1Address = csvData1[exec.scenario.iterationInTest % csvData1.length]
 
   group('B02_Address_01_AddressCRIEntryFromStub  GET', () => {
@@ -102,97 +100,63 @@ export function addressScenario1 (): void {
     isStatusCode200(res) && validatePageContent(res, 'Find your address')
       ? transactionDuration.add(endTime - startTime)
       : fail('Response Validation Failed')
-    csrfToken = getCSRF(res)
   })
 
   sleep(Math.random() * 3)
 
   group('B02_Address_02_SearchPostCode POST', () => {
     const startTime = Date.now()
-    res = http.post(
-      env.addressEndPoint + '/search',
-      {
-        addressSearch: user1Address.postcode,
-        continue: '',
-        'x-csrf-token': csrfToken
-      },
-      {
-        tags: { name: 'B02_Address_02_SearchPostCode' }
-      }
-    )
+    res = res.submitForm({
+      fields: { addressSearch: user1Address.postcode },
+      submitSelector: '#continue',
+      params: { tags: { name: 'B02_Address_02_SearchPostCode' } }
+    })
     const endTime = Date.now()
     isStatusCode200(res) && validatePageContent(res, 'Choose your address')
       ? transactionDuration.add(endTime - startTime)
       : fail('Response Validation Failed')
 
     fullAddress = res.html().find('select[name=addressResults]>option').last().val() ?? fail('Address not found')
-    csrfToken = getCSRF(res)
   })
 
   group('B02_Address_03_SelectAddress POST', () => {
     const startTime = Date.now()
-    res = http.post(
-      env.addressEndPoint + '/results',
-      {
-        addressResults: fullAddress,
-        continue: '',
-        'x-csrf-token': csrfToken
-      },
-      {
-        tags: { name: 'B02_Address_03_SelectAddress' }
-      }
-    )
+    res = res.submitForm({
+      fields: { addressResults: fullAddress },
+      submitSelector: '#continue',
+      params: { tags: { name: 'B02_Address_03_SelectAddress' } }
+    })
     const endTime = Date.now()
     isStatusCode200(res) && validatePageContent(res, 'Check your address')
       ? transactionDuration.add(endTime - startTime)
       : fail('Response Validation Failed')
-
-    addDetails = getAddressDetails(res)
-    csrfToken = getCSRF(res)
   })
 
   sleep(Math.random() * 3)
 
   group('B02_Address_04_VerifyAddress POST', () => {
     const startTime = Date.now()
-    res = http.post(
-      env.addressEndPoint + '/address',
-      {
-        addressFlatNumber: addDetails.flatNumber,
-        addressHouseNumber: addDetails.houseNumber,
-        addressHouseName: addDetails.houseName,
-        adddressStreetName: addDetails.streetName,
-        addressLocality: addDetails.town,
-        addressYearFrom: '2021',
-        continue: '',
-        'x-csrf-token': csrfToken
-      },
-      {
-        tags: { name: 'B02_Address_04_VerifyAddress' }
-      }
-    )
+    res = res.submitForm({
+      fields: { addressYearFrom: '2021' },
+      submitSelector: '#continue',
+      params: { tags: { name: 'B02_Address_04_VerifyAddress' } }
+    })
     const endTime = Date.now()
     isStatusCode200(res) && validatePageContent(res, 'Confirm your details')
       ? transactionDuration.add(endTime - startTime)
       : fail('Response Validation Failed')
-
-    csrfToken = getCSRF(res)
   })
 
   sleep(Math.random() * 3)
 
   group('B02_Address_05_ConfirmDetails POST', () => {
     const startTime1 = Date.now()
-    res = http.post(
-      env.addressEndPoint + '/summary/confirm',
-      {
-        'x-csrf-token': csrfToken
-      },
-      {
+    res = res.submitForm({
+      params: {
         redirects: 1,
-        tags: { name: 'B02_Address_05_ConfirmDetails_AddCRICall' }
+        tags: { name: 'B02_Address_05_ConfirmDetails_AddCRI' }
       }
-    )
+    })
     const endTime1 = Date.now()
     isStatusCode302(res)
       ? transactionDuration.add(endTime1 - startTime1)
@@ -202,7 +166,7 @@ export function addressScenario1 (): void {
     res = http.get(res.headers.Location,
       {
         headers: { Authorization: `Basic ${encodedCredentials}` },
-        tags: { name: 'B02_Address_05_ConfirmDetails_CoreStubCall' }
+        tags: { name: 'B02_Address_05_ConfirmDetails_CoreStub' }
       }
     )
     const endTime2 = Date.now()
@@ -210,27 +174,4 @@ export function addressScenario1 (): void {
       ? transactionDuration.add(endTime2 - startTime2)
       : fail('Response Validation Failed')
   })
-}
-
-function getCSRF (r: Response): string {
-  return r.html().find("input[name='x-csrf-token']").val() ?? ''
-}
-
-interface AddressDetails {
-  flatNumber: string
-  houseNumber: string
-  houseName: string
-  streetName: string
-  town: string
-}
-
-function getAddressDetails (r: Response): AddressDetails {
-  const html = r.html()
-  return {
-    flatNumber: html.find("input[name='addressFlatNumber']").val() ?? '',
-    houseNumber: html.find("input[name='addressHouseNumber']").val() ?? '',
-    houseName: html.find("input[name='addressHouseName']").val() ?? '',
-    streetName: html.find("input[name='addressStreetName']").val() ?? '',
-    town: html.find("input[name='addressLocality']").val() ?? ''
-  }
 }

a657d71fd0295d1ea764f81e79ad6cdcf5d79ddd
Abhinay Srivastava <abhinay.srivastava@digital.cabinet-office.gov.uk>
2023-08-17 18:39:56 +0100

    Updated IPVR payload

--- deploy/scripts/src/cri-kiwi/requestGenerator/ipvrReqFormat.ts
+++ deploy/scripts/src/cri-kiwi/requestGenerator/ipvrReqFormat.ts
@@ -1,61 +1,44 @@
 export interface AUTH_IPV_AUTHORISATION_REQUESTED {
   event_id: string
   client_id: string
-  component_id: string
+  clientLandingPageUrl: string
   event_name: string
-  extensions: {
-    rpPairwiseId: string
-  }
+  rp_name: string
   timestamp: string
   timestamp_formatted: string
   user: {
     user_id: string
     email: string
-    ip_address: string
-    session_id: string
-    persistent_session_id: string
     govuk_signin_journey_id: string
-  }
-  txma: {
-    obfuscated: boolean
   }
 }
 
 export interface F2F_YOTI_START {
+  event_id: string
+  client_id: string
+  clientLandingPageUrl: string
   event_name: string
+  rp_name: string
+  timestamp: string
+  timestamp_formatted: string
   user: {
     user_id: string
-    session_id: string
     govuk_signin_journey_id: string
-  }
-  client_id: string
-  timestamp: string
-  component_id: string
-  extensions: {
-    evidence: [
-      {
-        txn: string
-      }
-    ]
-    post_office_details: {
-      address: string
-      location: {
-        latitude: number
-        longitude: number
-      }
-      post_code: string
-    }
-  }
-  restricted: {
-    documentType: string
-    issuingCountry: string
   }
 }
 
 export interface IPV_F2F_CRI_VC_CONSUMED {
   event_id: string
-  component_id: string
+  client_id: string
+  clientLandingPageUrl: string
   event_name: string
+  rp_name: string
+  timestamp: string
+  timestamp_formatted: string
+  user: {
+    user_id: string
+    govuk_signin_journey_id: string
+  }
   restricted: {
     nameParts: [
       {
@@ -65,16 +48,11 @@ export interface IPV_F2F_CRI_VC_CONSUMED {
       {
         type: string
         value: string
+      },
+      {
+        type: string
+        value: string
       }
     ]
-  }
-  timestamp: string
-  timestamp_formatted: string
-  user: {
-    user_id: string
-    govuk_signin_journey_id: string
-  }
-  txma: {
-    obfuscated: boolean
   }
 }

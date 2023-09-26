6f387223cc9e9f34797b2619733f3ba856cc4b6e
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2023-09-26 11:52:00 +0100

    Add JSDoc comment to common/utils/sleep function

--- deploy/scripts/src/common/utils/sleep/sleepBetween.ts
+++ deploy/scripts/src/common/utils/sleep/sleepBetween.ts
@@ -1,5 +1,14 @@
 import { sleep } from 'k6'
 
+/**
+ * Utility to `sleep` a VU for a random time between a given minimum and
+ * maximum duration (inclusive)
+ * @param min Minimum time to sleep (in seconds)
+ * @param max Maximum time to sleep (in seconds)
+ * @example
+ * sleep(3, 5) // Sleeps for between 1 to 3 seconds
+ * sleep(0.5, 1.5) // Sleeps for between half a second and 1.5 seconds
+ */
 export function sleepBetween (min: number, max: number): void {
   sleep(min + Math.random() * Math.abs(max - min))
 }

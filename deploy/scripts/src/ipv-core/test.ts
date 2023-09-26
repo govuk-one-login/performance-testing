411c9b84df05add0da16211dc6a62387fa664192
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2023-09-26 15:22:17 +0100

    Update ipv-core/test.ts

--- deploy/scripts/src/ipv-core/test.ts
+++ deploy/scripts/src/ipv-core/test.ts
@@ -1,4 +1,4 @@
-import { sleep, group } from 'k6'
+import { group } from 'k6'
 import { type Options } from 'k6/options'
 import http, { type Response } from 'k6/http'
 import { selectProfile, type ProfileList, describeProfile } from '../common/utils/config/load-profiles'
@@ -6,6 +6,8 @@ import { getStaticResources } from '../common/utils/request/static'
 import { timeRequest } from '../common/utils/request/timing'
 import { passportPayload, addressPayloadP, kbvPayloadP, fraudPayloadP } from './data/passportData'
 import { addressPayloadDL, kbvPayloaDL, fraudPayloadDL, drivingLicencePayload } from './data/drivingLicenceData'
+import { isStatusCode200, isStatusCode302, pageContentCheck } from '../common/utils/checks/assertions'
+import { sleepBetween } from '../common/utils/sleep/sleepBetween'
 
 const profiles: ProfileList = {
   smoke: {
@@ -117,12 +119,9 @@ export function passport (): void {
     timeRequest(() => http.get(env.orchStubEndPoint, {
       tags: { name: 'B01_Passport_01_LaunchOrchestratorStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Enter userId manually')
-    }))
+    { isStatusCode200, ...pageContentCheck('Enter userId manually') }))
 
-  sleep(Math.random() * 2)
+  sleepBetween(1, 2)
 
   res = group('B01_Passport_02_GoToFullJourneyRoute GET', () =>
     timeRequest(() => {
@@ -130,12 +129,9 @@ export function passport (): void {
       if (env.staticResources) getStaticResources(response)
       return response
     },
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Tell us if you have one of the following types of photo ID')
-    }))
+    { isStatusCode200, ...pageContentCheck('Tell us if you have one of the following types of photo ID') }))
 
-  sleep(Math.random() * 2)
+  sleepBetween(1, 2)
 
   group('B01_Passport_03_ClickContinueStartPage POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -145,19 +141,14 @@ export function passport (): void {
         tags: { name: 'B01_Passport_03_ClickContinueStartPage_01_CoreCall' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       tags: { name: 'B01_Passport_03_ClickContStartPage_02_DCMAWStub' } // pragma: allowlist secret
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('DOC Checking App (Stub)')
-    })
+    { isStatusCode200, ...pageContentCheck('DOC Checking App (Stub)') })
   })
 
-  sleep(Math.random() * 2)
+  sleepBetween(1, 2)
 
   group('B01_Passport_04_DCMAWContinue POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -170,19 +161,14 @@ export function passport (): void {
         tags: { name: 'B01_Passport_04_DCMAWContinue_01_DCMAWStub' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(env.ipvCoreURL + res.headers.Location, {
       tags: { name: 'B01_Passport_04_DCMAWContinue_02_CoreCall' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Enter your UK passport details and answer security questions online')
-    })
+    { isStatusCode200, ...pageContentCheck('Enter your UK passport details and answer security questions online') })
   })
 
-  sleep(Math.random() * 2)
+  sleepBetween(1, 2)
 
   group('B01_Passport_05_ContinueOnPYIStartPage POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -192,17 +178,12 @@ export function passport (): void {
         tags: { name: 'B01_Passport_05_ContOnPYIStartPage_01_CoreCall' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, { tags: { name: 'B01_Passport_05_ContOnPYIStartPage_02_PassStub' } }),
-      {
-        'is status 200': (r) => r.status === 200,
-        'verify page content': (r) => (r.body as string).includes('UK Passport (Stub)')
-      })
+      { isStatusCode200, ...pageContentCheck('UK Passport (Stub)') })
   })
 
-  sleep(Math.random() * 2)
+  sleepBetween(1, 2)
 
   group('B01_Passport_06_PassportDataContinue POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -216,28 +197,21 @@ export function passport (): void {
         tags: { name: 'B01_Passport_06_PassDataContinue_01_PassStub' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location,
       {
         redirects: 0,
         tags: { name: 'B01_Passport_06_PassDataContinue_02_CoreCall' }
       }
     ),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       tags: { name: 'B01_Passport_06_PassDataContinue_03_AddStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Address (Stub)')
-    })
+    { isStatusCode200, ...pageContentCheck('Address (Stub)') })
   })
 
-  sleep(Math.random() * 2)
+  sleepBetween(1, 2)
 
   group('B01_Passport_07_AddrDataContinue POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -247,27 +221,20 @@ export function passport (): void {
         tags: { name: 'B01_Passport_07_AddrDataContinue_01_AddStub' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location,
       {
         redirects: 0,
         tags: { name: 'B01_Passport_07_AddrDataContinue_02_CoreCall' }
       }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       tags: { name: 'B01_Passport_07_AddrDataContinue_03_FraudStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Fraud Check (Stub)')
-    })
+    { isStatusCode200, ...pageContentCheck('Fraud Check (Stub)') })
   })
 
-  sleep(Math.random() * 2)
+  sleepBetween(1, 2)
 
   group('B01_Passport_08_FraudDataContinue POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -280,21 +247,16 @@ export function passport (): void {
         tags: { name: 'B01_Passport_08_FraudDataContinue_01_FraudStub' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => {
       const response = http.get(res.headers.Location, { tags: { name: 'B01_Passport_08_FraudDataContinue_02_CoreCall' } })
       if (env.staticResources) getStaticResources(response)
       return response
     },
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Answer security questions')
-    })
+    { isStatusCode200, ...pageContentCheck('Answer security questions') })
   })
 
-  sleep(Math.random() * 2)
+  sleepBetween(1, 2)
 
   group('B01_Passport_09_PreKBVTransition POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -303,19 +265,14 @@ export function passport (): void {
         tags: { name: 'B01_Passport_09_PreKBVTransition_01_CoreCall' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       tags: { name: 'B01_Passport_09_PreKBVTransition_02_KBVStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Knowledge Based Verification (Stub)')
-    })
+    { isStatusCode200, ...pageContentCheck('Knowledge Based Verification (Stub)') })
   })
 
-  sleep(Math.random() * 2)
+  sleepBetween(1, 2)
 
   group('B01_Passport_10_KBVDataContinue POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -328,21 +285,16 @@ export function passport (): void {
         tags: { name: 'B01_Passport_10_KBVDataContinue_01_KBVStub' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => {
       const response = http.get(res.headers.Location, { tags: { name: 'B01_Passport_10_KBVDataContinue_02_CoreCall' } })
       if (env.staticResources) getStaticResources(response)
       return response
     },
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('You’ve successfully proved your identity')
-    })
+    { isStatusCode200, ...pageContentCheck('You’ve successfully proved your identity') })
   })
 
-  sleep(Math.random() * 2)
+  sleepBetween(1, 2)
 
   group('B01_Passport_11_ContinuePYISuccessPage POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -351,16 +303,11 @@ export function passport (): void {
         tags: { name: 'B01_Passport_11_ContPYISuccessPage_01_CoreCall' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       tags: { name: 'B01_Passport_11_ContPYISuccessPage_02_OrchStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('User information')
-    })
+    { isStatusCode200, ...pageContentCheck('User information') })
   })
 }
 
@@ -371,12 +318,9 @@ export function drivingLicence (): void {
     timeRequest(() => http.get(env.orchStubEndPoint, {
       tags: { name: 'B02_DrivingLicence_01_LaunchOrchestratorStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Enter userId manually')
-    }))
+    { isStatusCode200, ...pageContentCheck('Enter userId manually') }))
 
-  sleep(Math.random() * 3)
+  sleepBetween(1, 3)
 
   res = group('B02_DrivingLicence_02_SelectUserIDContinue GET', () =>
     timeRequest(() => {
@@ -384,12 +328,9 @@ export function drivingLicence (): void {
       if (env.staticResources) getStaticResources(response)
       return response
     },
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Tell us if you have one of the following types of photo ID')
-    }))
+    { isStatusCode200, ...pageContentCheck('Tell us if you have one of the following types of photo ID') }))
 
-  sleep(Math.random() * 3)
+  sleepBetween(1, 3)
 
   group('B02_DrivingLicence_03_ContinueStartPage POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -399,19 +340,14 @@ export function drivingLicence (): void {
         tags: { name: 'B02_DrivingLicence_03_ContStartPage_01_CoreCall' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       tags: { name: 'B02_DrivingLicence_03_ContStartPage_02_DCMAWStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('DOC Checking App (Stub)')
-    })
+    { isStatusCode200, ...pageContentCheck('DOC Checking App (Stub)') })
   })
 
-  sleep(Math.random() * 3)
+  sleepBetween(1, 3)
 
   group('B02_DrivingLicence_04_DCMAWContinue POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -424,18 +360,13 @@ export function drivingLicence (): void {
         tags: { name: 'B02_DrivingLicence_04_DCMAWContinue_01_DCMAWStub' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => {
       const response = http.get(env.ipvCoreURL + res.headers.Location, { tags: { name: 'B02_DrivingLicence_04_DCMAWContinue_02_CoreCall' } })
       if (env.staticResources) getStaticResources(response)
       return response
     },
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Enter your UK photocard driving licence details and answer security questions online')
-    })
+    { isStatusCode200, ...pageContentCheck('Enter your UK photocard driving licence details and answer security questions online') })
   })
 
   group('B02_DrivingLicence_05_ContinueOnPYIStartPage POST', () => {
@@ -446,16 +377,11 @@ export function drivingLicence (): void {
         tags: { name: 'B02_DrivingLicence_05_ContPYIStartPage_01_CoreCall' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       tags: { name: 'B02_DrivingLicence_05_ContPYIStartPage_02_DLStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Driving Licence (Stub)')
-    })
+    { isStatusCode200, ...pageContentCheck('Driving Licence (Stub)') })
   })
 
   group('B02_DrivingLicence_06_DrivingLicenceDataContinue POST', () => {
@@ -471,26 +397,19 @@ export function drivingLicence (): void {
         tags: { name: 'B02_DrivingLicence_06_DLDataContinue_01_DLStub' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       redirects: 0,
       tags: { name: 'B02_DrivingLicence_06_DLDataContinue_02_CoreCall' }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       tags: { name: 'B02_DrivingLicence_06_DLDataContinue_03_AddStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Address (Stub)')
-    })
+    { isStatusCode200, ...pageContentCheck('Address (Stub)') })
   })
 
-  sleep(Math.random() * 3)
+  sleepBetween(1, 3)
 
   group('B02_DrivingLicence_07_AddrDataContinue POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -500,26 +419,19 @@ export function drivingLicence (): void {
         tags: { name: 'B02_DrivingLicence_07_AddrDataCont_01_AddStub' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       redirects: 0,
       tags: { name: 'B02_DrivingLicence_07_AddrDataCont_02_CoreCall' }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       tags: { name: 'B02_DrivingLicence_07_AddrDataCont_03_FraudStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Fraud Check (Stub)')
-    })
+    { isStatusCode200, ...pageContentCheck('Fraud Check (Stub)') })
   })
 
-  sleep(Math.random() * 3)
+  sleepBetween(1, 3)
 
   group('B02_DrivingLicence_08_FraudDataContinue POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -533,21 +445,16 @@ export function drivingLicence (): void {
         tags: { name: 'B02_DrivingLicence_08_FraudDataCont_01_FraudStub' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => {
       const response = http.get(res.headers.Location, { tags: { name: 'B02_DrivingLicence_08_FraudDataCont_02_CoreCall' } })
       if (env.staticResources) getStaticResources(response)
       return response
     },
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Answer security questions')
-    })
+    { isStatusCode200, ...pageContentCheck('Answer security questions') })
   })
 
-  sleep(Math.random() * 3)
+  sleepBetween(1, 3)
 
   group('B02_DrivingLicence_09_PreKBVTransition POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -556,19 +463,14 @@ export function drivingLicence (): void {
         tags: { name: 'B02_DrivingLicence_09_PreKBVTransition_01_CoreCall' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       tags: { name: 'B02_DrivingLicence_09_PreKBVTransition_02_KBVStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Knowledge Based Verification (Stub)')
-    })
+    { isStatusCode200, ...pageContentCheck('Knowledge Based Verification (Stub)') })
   })
 
-  sleep(Math.random() * 3)
+  sleepBetween(1, 3)
 
   group('B02_DrivingLicence_10_KBVDataContinue POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -581,21 +483,16 @@ export function drivingLicence (): void {
         tags: { name: 'B02_DrivingLicence_10_KBVDataContinue_01_KBVStub' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => {
       const response = http.get(res.headers.Location, { tags: { name: 'B02_DrivingLicence_10_KBVDataContinue_02_CoreCall' } })
       if (env.staticResources) getStaticResources(response)
       return response
     },
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('Continue to the service you want to use')
-    })
+    { isStatusCode200, ...pageContentCheck('Continue to the service you want to use') })
   })
 
-  sleep(Math.random() * 3)
+  sleepBetween(1, 3)
 
   group('B02_DrivingLicence_11_ContinueDrivingLicenceSuccessPage POST', () => {
     res = timeRequest(() => res.submitForm({
@@ -604,15 +501,10 @@ export function drivingLicence (): void {
         tags: { name: 'B02_DrivingLicence_11_ContDLSuccess_01_CoreCall' }
       }
     }),
-    {
-      'is status 302': (r) => r.status === 302
-    })
+    { isStatusCode302 })
     res = timeRequest(() => http.get(res.headers.Location, {
       tags: { name: 'B02_DrivingLicence_11_ContDLSuccess_02_OrchStub' }
     }),
-    {
-      'is status 200': (r) => r.status === 200,
-      'verify page content': (r) => (r.body as string).includes('User information')
-    })
+    { isStatusCode200, ...pageContentCheck('User information') })
   })
 }

e851344d847a6a7c8d7fad0477743165e46be4d4
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2023-09-26 11:50:49 +0100

    Update common/checks/assertion functions

--- deploy/scripts/src/common/utils/checks/assertions.ts
+++ deploy/scripts/src/common/utils/checks/assertions.ts
@@ -1,14 +1,48 @@
 import { type Response } from 'k6/http'
-import { check } from 'k6'
+import { type Checkers } from 'k6'
 
-export function checkStatusCode (statusCode: number, res: Response): boolean {
-  return check(res, {
-    'HTTP Response Status Validation': (r) => r.status === statusCode
-  })
+/**
+ * Function to check that a `Response` has status code 200
+ * @param response - `Response` to check that status code of
+ * @returns `true` if status code is `200`, `false` otherwise
+ * @example
+ * const res = timeRequest(
+ *   () => http.get(url),
+ *   { isStatusCode200 }
+ * )
+ */
+export function isStatusCode200 (response: Response): boolean {
+  return response.status === 200
 }
 
-export function validatePageContent (res: Response, pageContent: string): boolean {
-  return check(res, {
-    'Validate page content': (r) => (r.body as string).includes(pageContent)
-  })
+/**
+ * Function to check that a `Response` has status code 302
+ * @param response - `Response` to check that status code of
+ * @returns `true` if status code is `302`, `false` otherwise
+ * @example
+ * const res = timeRequest(
+ *   () => http.get(url),
+ *   { isStatusCode302 }
+ * )
+ */
+export function isStatusCode302 (response: Response): boolean {
+  return response.status === 302
+}
+
+/**
+ * Generates a `Checkers<Response>` Object to validate the content of a page
+ * includes the given string
+ * @param content - String that the page is expected to contain
+ * @returns `Checkers<Response>` object containing one named `Checker<Response`
+ * function
+ * @example
+ * const res = timeRequest(
+ *   () => http.get(url),
+ *   { ...pageContentCheck('Enter your email address') }
+ * )
+ */
+export function pageContentCheck (content: string): Checkers<Response> {
+  return {
+    validatePageContent: r => (r.body as string).includes(content)
+  }
 }

21c2c5261cb9255b6994443757a1e29f91a31080
Abhinay Srivastava <abhinay.srivastava@digital.cabinet-office.gov.uk>
2023-10-20 09:44:04 +0100

    Incorporated review comments

--- deploy/scripts/src/common/utils/custom_metric/counter.ts
+++ deploy/scripts/src/common/utils/custom_metric/counter.ts
@@ -1,4 +1,4 @@
 import { Counter } from 'k6/metrics'
 
-export const iterationsStarted = new Counter('Iterations_Started')
-export const iterationsCompleted = new Counter('Iterations_Completed')
+export const iterationsStarted = new Counter('iterations_started')
+export const iterationsCompleted = new Counter('iterations_completed')

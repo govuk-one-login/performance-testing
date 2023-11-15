fe0f7b933d5d12d1e45e3e120780f6a5719591a2
Abhinay Srivastava <abhinay.srivastava@digital.cabinet-office.gov.uk>
2023-11-15 17:40:20 +0000

    Updated JSDOC comments for SigV4

--- deploy/scripts/src/common/utils/jslib/aws-signature.d.ts
+++ deploy/scripts/src/common/utils/jslib/aws-signature.d.ts
@@ -6,6 +6,29 @@ export class SignatureV4 {
    * The presign operation produces a pre-signed request with authorization information stored in its query
    * string parameters
    * https://k6.io/docs/javascript-api/jslib/aws/
+   *
+   * @param {string} service The AWS service to sign or pre-sign requests for
+   * @param {string} region The AWS region to sign or pre-sign requests for
+   * @param {object} credentials The AWS credentials to sign or pre-sign requests with.
+   *                             An object with accessKeyId, secretAccessKeyId,
+   *                             and optional sessionToken properties
+   * @param {boolean} uriEscapePath Whether to uri-escape the request URI path
+   *                                as part of computing the canonical request string
+   * @param {boolean} applyChecksum Whether to calculate a checksum of the request body
+   *                                and include it as either a request header (when signing)
+   *                                or as a query string parameter (when pre-signing)
+   * @example
+   * const signer = new SignatureV4({
+   *  service: 's3',
+   *  region: awsConfig.region,
+   *  credentials: {
+   *    accessKeyId: awsConfig.accessKeyId,
+   *    secretAccessKey: awsConfig.secretAccessKey,
+   *    sessionToken: awsConfig.sessionToken,
+   * },
+   * uriEscapePath: false,
+   * applyChecksum: false,
+   * })
    */
   constructor (options: {
     service: string
@@ -22,6 +45,27 @@ export class SignatureV4 {
   /**
    * SignatureV4.sign() signs an HTTP request with the AWS Signature V4 algorithm.
    * https://k6.io/docs/javascript-api/jslib/aws/signaturev4/sign/
+   *
+   * @param {string} method The HTTP method of the request
+   * @param {string} protocol The network protocol of the request. Accepted values 'http' and 'https'
+   * @param {string} hostname The hostname the request is sent to
+   * @param {string} path The path of the request
+   * @param {object} headers The headers of the HTTP request
+   * @param {string or ArrayBuffer} body The optional body of the HTTP request
+   * @param {object} query The optional query parameters of the HTTP request
+   * @example
+   * const signedRequest = signer.sign(
+   *  method: 'GET',
+   *  protocol: 'https',
+   *  hostname: 'mybucket.s3.us-east-1.amazonaws.com',
+   *  path: '/myfile.txt',
+   *  headers: {},
+   * },
+   * {
+   * signingDate: new Date(),
+   * signingService: 's3',
+   * signingRegion: 'us-east-1',
+   * })
    */
   sign (request: {
     method: string

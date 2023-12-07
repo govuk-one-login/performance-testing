5d06d27c2bb00b712beb1de4d8da89e441d53ed6
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2023-12-07 11:47:28 +0000

    Add logic for switching environments

--- deploy/scripts/src/mobile/utils/config.ts
+++ deploy/scripts/src/mobile/utils/config.ts
@@ -1,7 +1,13 @@
+import { fail } from 'k6'
+
 // __ENV is the syntax in k6 for accessing environment variables
 // Refer to deploy/scripts/README.md for guidance on how to set environment variables
+export const environment = __ENV.ENVIRONMENT.toLocaleUpperCase()
+const validEnvironments = ['BUILD', 'DEV']
+if (environment in validEnvironments) fail(`Environment '${environment}' not in ${validEnvironments.toString()}`)
+
 export const config = {
-  testClientExecuteUrl: __ENV.MOBILE_TEST_CLIENT_EXECUTE_URL,
-  backendUrl: __ENV.MOBILE_BACKEND_URL,
-  frontendUrl: __ENV.MOBILE_FRONTEND_URL
+  testClientExecuteUrl: __ENV[`MOBILE_${environment}_TEST_CLIENT_URL`],
+  backendUrl: __ENV[`MOBILE_${environment}_BACKEND_URL`],
+  frontendUrl: __ENV[`MOBILE_${environment}_FRONTEND_URL`]
 }

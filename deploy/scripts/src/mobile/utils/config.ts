6881e43fac644797cbbf72d08833694ce3040b19
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2023-12-07 12:32:16 +0000

    Fix environment error handling logic

--- deploy/scripts/src/mobile/utils/config.ts
+++ deploy/scripts/src/mobile/utils/config.ts
@@ -1,10 +1,8 @@
-import { fail } from 'k6'
-
 // __ENV is the syntax in k6 for accessing environment variables
 // Refer to deploy/scripts/README.md for guidance on how to set environment variables
 export const environment = __ENV.ENVIRONMENT.toLocaleUpperCase()
 const validEnvironments = ['BUILD', 'DEV']
-if (environment in validEnvironments) fail(`Environment '${environment}' not in ${validEnvironments.toString()}`)
+if (!validEnvironments.includes(environment)) throw new Error(`Environment '${environment}' not in [${validEnvironments.toString()}]`)
 
 export const config = {
   testClientExecuteUrl: __ENV[`MOBILE_${environment}_TEST_CLIENT_URL`],

6d21c89cd67d40714d2ef1ff36c09efaac8dafd4
Raluca Alecu <raluca-elena.alecu@digital.cabinet-office.gov.uk>
2024-01-19 15:05:08 +0200

    Added incremental load scenario

--- deploy/scripts/src/mobile/frontend-e2e.test.ts
+++ deploy/scripts/src/mobile/frontend-e2e.test.ts
@@ -65,6 +65,26 @@ const profiles: ProfileList = {
       maxVUs: 75, // Calculation: 1 journeys / second * 24 seconds maximum journey time + 50 buffer
       exec: 'mamIphonePassport'
     }
+  },
+  incrementalLoad: {
+    mamIphonePassport: {
+      executor: 'ramping-arrival-rate',
+      startRate: 1,
+      timeUnit: '1s',
+      preAllocatedVUs: 1700, // Calculation: 100 journeys / second * 17 seconds average journey time
+      maxVUs: 3000, // Calculation: 100 journeys / second * 2.5 seconds maximum expected from NFR (2.5 per request, 10 user-facing requests + safety)
+      stages: [
+        { target: 25, duration: '4m' }, // linear increase from 0 iteration per second to 25 iterations per second for 4 mins -> 0.1 t/s/s
+        { target: 25, duration: '10m' }, // maintain 25 iterations per second for 10 min
+        { target: 50, duration: '4m' }, // linear increase from 0 iteration per second to 50 iterations per second for 4 mins -> 0.1 t/s/s
+        { target: 50, duration: '10m' }, // maintain 50 iterations per second for 10 min
+        { target: 75, duration: '4m' }, // linear increase from 0 iteration per second to 75 iterations per second for 4 mins -> 0.1 t/s/s
+        { target: 75, duration: '10m' }, // maintain 75 iterations per second for 10 min
+        { target: 100, duration: '4m' }, // linear increase from 0 iteration per second to 100 iterations per second for 4 mins -> 0.1 t/s/s
+        { target: 100, duration: '10m' } // maintain 100 iterations per second for 10 min
+      ],
+      exec: 'mamIphonePassport'
+    }
   }
 }
 

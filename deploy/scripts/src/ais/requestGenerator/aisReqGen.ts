56d8193a4813aabbf16ba63f5419dbd2312f6303
Abhinay Srivastava <abhinay.srivastava@digital.cabinet-office.gov.uk>
2024-01-31 15:02:41 +0000

    Changes based on latest review comments

--- deploy/scripts/src/ais/requestGenerator/aisReqGen.ts
+++ deploy/scripts/src/ais/requestGenerator/aisReqGen.ts
@@ -1,5 +1,24 @@
-import { type TicfAccountIntervention } from '../requestGenerator/aisReqFormat'
 import { uuidv4 } from '../../common/utils/jslib/index'
+
+interface TicfAccountIntervention {
+  timestamp: number
+  event_timestamp_ms: number
+  event_name: string
+  event_id: string
+  component_id: string
+  user: {
+    user_id: string
+  }
+  extensions: {
+    intervention: {
+      intervention_code: string
+      intervention_reason: string
+      originating_component_id: string
+      originator_reference_id: string
+      requester_id: string
+    }
+  }
+}
 
 export function generatePersistIVRequest (userID: string, interventionCode: string): TicfAccountIntervention {
   return {

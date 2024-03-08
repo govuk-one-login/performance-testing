9962368ccea320237f79e485114be75e1ca45ba1
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-03-08 17:47:34 +0000

    Add thresholds utility file

---
+++ deploy/scripts/src/common/utils/config/thresholds.ts
@@ -0,0 +1,48 @@
+import { type Threshold, type Options } from 'k6/options'
+import { defaultConfig } from './load-profiles'
+
+export interface NFRs {
+  errors: Threshold[]
+  rt: Threshold[]
+}
+export const nfrs: NFRs = {
+  errors: ['rate<0.05'], // Error rate <5%
+  rt: [
+    'p(95)<=1000', // 95th percentile response time <=1000ms
+    'p(99)<=2500' //  99th percentile response time <=2500ms
+  ]
+}
+export type Thresholds = Options['thresholds']
+export type GroupMap = Record<string, string[]>
+
+/**
+ * get
+ */
+export function getThresholds (groupMap: GroupMap, selections: string | undefined = defaultConfig.scenario): Thresholds {
+  const thresholds: Thresholds = {
+    http_req_duration: nfrs.rt,
+    http_req_failed: nfrs.errors
+  }
+  const addThresholds = (names: string[]): void => {
+    names.forEach(name => {
+      thresholds[`duration{group:::${name}}`] = nfrs.rt
+    })
+  }
+
+  if (selections == null || selections === '' || selections.toLowerCase() === 'all') { // Enable all scenarios is selection string is null, empty or set to 'all'
+    Object.values(groupMap).forEach(groupNames => {
+      addThresholds(groupNames)
+    })
+  } else {
+    selections.split(',').forEach(scenario => {
+      const groupNames = groupMap[scenario]
+      if (groupNames === undefined) {
+        console.warn(`Warning: Scenario '${scenario}' does not exist in group name map. Duration thresholds by group will not be applied`)
+        return
+      }
+      addThresholds(groupNames)
+    })
+  }
+
+  return thresholds
+}

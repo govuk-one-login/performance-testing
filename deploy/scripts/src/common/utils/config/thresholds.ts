b4746bc00a382d2f7545b5fa924c6215a83a9e60
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-03-11 10:55:11 +0000

    JSDoc documentation for `getThresholds`

--- deploy/scripts/src/common/utils/config/thresholds.ts
+++ deploy/scripts/src/common/utils/config/thresholds.ts
@@ -16,7 +16,25 @@ export type Thresholds = Options['thresholds']
 export type GroupMap = Record<string, readonly string[]>
 
 /**
- * TODO: Add JSDocs
+ * Generates thresholds for response times against NFRs given the names of groups being executed in the test
+ * @param {GroupMap} groupMap - An object which maps scenario names to arrays of group names within each scenario
+ * @param {string | undefined} [selections] - Optional scenario selection string. When not specified this will use the
+ * `__ENV.SCENARIO`, or if also not specified, default to all scenarios enabled
+ * @returns Thresholds object which can be used in the k6 `options` object to specify test thresholds
+ * @example
+ * const groupMap = {
+ *   scenario1: [
+ *     'group1a'
+ *   ],
+ *   scenario2: [
+ *     'group2a',
+ *     'group2b'
+ *   ]
+ * } as const
+ *
+ * export const options: Options = {
+ *   thresholds: getThresholds(groupMap)
+ * }
  */
 export function getThresholds (groupMap: GroupMap, selections: string | undefined = defaultConfig.scenario): Thresholds {
   const thresholds: Thresholds = {

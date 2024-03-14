e6480c088071379f335cb37efbba981529e5399f
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-03-14 15:49:17 +0000

    Add createScenario() function

--- deploy/scripts/src/common/utils/config/load-profiles.ts
+++ deploy/scripts/src/common/utils/config/load-profiles.ts
@@ -1,4 +1,4 @@
-import { type Scenario } from 'k6/options'
+import { type Stage, type Scenario } from 'k6/options'
 
 export type ScenarioList = Record<string, Scenario>
 export type ProfileList = Record<string, ScenarioList>
@@ -53,4 +53,70 @@ export function selectProfile (profiles: ProfileList, config: typeof defaultConf
 export function describeProfile (profile: Profile): void {
   console.log(`Load Profile: <\x1b[32m${profile.name}\x1b[0m>`)
   console.log(`Scenarios: <\x1b[34m${Object.keys(profile.scenarios).join('\x1b[0m|\x1b[34m')}\x1b[0m>`)
+}
+
+export enum LoadProfile {
+  smoke,
+  short,
+  full,
+  deployment
+}
+function createStages (type: LoadProfile, target: number): Stage[] {
+  switch (type) {
+    case LoadProfile.smoke:
+      return [
+        { target, duration: '60s' } // 1 minute smoke test
+      ]
+    case LoadProfile.short:
+      return [
+        { target, duration: '5m' }, // Ramps up to target throughput in 5 minutes
+        { target, duration: '15m' }, // Maintain steady state at target throughput for 15 minutes
+        { target: 0, duration: '5m' } // Ramp down over 5 minutes
+      ]
+    case LoadProfile.full:
+      return [
+        { target, duration: '15m' }, // Ramps up to target throughput in 15 minutes
+        { target, duration: '30m' }, // Maintain steady state at target throughput for 30 minutes
+        { target: 0, duration: '5m' } // Ramp down over 5 minutes
+      ]
+    case LoadProfile.deployment:
+      return [
+        { target, duration: '5m' }, // Ramp up to target throughput over 5 minutes
+        { target, duration: '20m' }, // Maintain steady state at target throughput for 20 minutes
+        { target: 0, duration: '5m' } // Ramp down over 5 minutes
+      ]
+  }
+}
+
+/**
+ * Creates a valid load profile scenario
+ * @param {string} exec Name of the scenario to execute, must match exported function in script. `Scenario.exec`
+ * @param {LoadProfile} type Enum of the load profile to use e.g. `LoadProfile.smoke`
+ * @param {number} [target] Target iterations per second, defaults to `1`
+ * @param {number} [duration] Max iteration duration in seconds, defaults to `30`
+ * @returns {ScenarioList} ScenarioList with the single scenario specified
+ * @example
+ * const profiles: ProfileList = {
+ *  smoke: {
+ *    ...createScenario('signIn', LoadProfile.smoke),
+ *    ...createScenario('signUp', LoadProfile.smoke)
+ *  },
+ *  load: {
+ *    ...createScenario('signIn', LoadProfile.full, 100, 15),
+ *    ...createScenario('signUp', LoadProfile.full, 20)
+ *  }
+ * }
+ */
+export function createScenario (exec: string, type: LoadProfile, target: number = 1, duration: number = 30): ScenarioList {
+  const list: ScenarioList = {}
+  list[exec] = {
+    executor: 'ramping-arrival-rate',
+    startRate: 1,
+    timeUnit: '1s',
+    preAllocatedVUs: target * duration / 2,
+    maxVUs: target * duration,
+    stages: createStages(type, target),
+    exec
+  }
+  return list
 }

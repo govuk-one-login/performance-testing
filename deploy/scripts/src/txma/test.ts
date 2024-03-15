e618572ce7ed2aef3ef9e77e6793e04081107f1a
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-03-15 10:57:59 +0000

    Standardise `txma` load profiles

--- deploy/scripts/src/txma/test.ts
+++ deploy/scripts/src/txma/test.ts
@@ -1,6 +1,6 @@
 import { iterationsStarted, iterationsCompleted } from '../common/utils/custom_metric/counter'
 import { type Options } from 'k6/options'
-import { selectProfile, type ProfileList, describeProfile } from '../common/utils/config/load-profiles'
+import { selectProfile, type ProfileList, describeProfile, createScenario, LoadProfile } from '../common/utils/config/load-profiles'
 import { uuidv4 } from '../common/utils/jslib/index.js'
 import { AWSConfig, SQSClient } from '../common/utils/jslib/aws-sqs'
 import { type AssumeRoleOutput } from '../common/utils/aws/types'
@@ -8,47 +8,13 @@ import { getEnv } from '../common/utils/config/environment-variables'
 
 const profiles: ProfileList = {
   smoke: {
-    sendEventSmokeTest: {
-      executor: 'ramping-arrival-rate',
-      startRate: 1,
-      timeUnit: '1s',
-      preAllocatedVUs: 1,
-      maxVUs: 1,
-      stages: [
-        { target: 1, duration: '60s' } // Ramps up to target load
-      ],
-      exec: 'sendEvent'
-    }
+    ...createScenario('sendEvent', LoadProfile.smoke)
   },
   load: {
-    sendEventScenario: {
-      executor: 'ramping-arrival-rate',
-      startRate: 1,
-      timeUnit: '1s',
-      preAllocatedVUs: 1,
-      maxVUs: 2250,
-      stages: [
-        { target: 750, duration: '15m' }, // Ramp up to 750 iterations per second in 15 minutes
-        { target: 750, duration: '30m' }, // Maintain steady state at 750 iterations per second for 30 minutes
-        { target: 0, duration: '5m' } // Total rap down in 5 minutes
-      ],
-      exec: 'sendEvent'
-    }
+    ...createScenario('sendEvent', LoadProfile.full, 750, 3)
   },
   stress: {
-    sendEventScenario: {
-      executor: 'ramping-arrival-rate',
-      startRate: 1,
-      timeUnit: '1s',
-      preAllocatedVUs: 1,
-      maxVUs: 22500,
-      stages: [
-        { target: 7500, duration: '15m' }, // Ramp up to 7500 iterations per second in 15 minutes
-        { target: 7500, duration: '30m' }, // Maintain steady state at 7500 iterations per second for 30 minutes
-        { target: 0, duration: '5m' } // Total ramp down in 5 minutes
-      ],
-      exec: 'sendEvent'
-    }
+    ...createScenario('sendEvent', LoadProfile.full, 7500, 3)
   }
 }
 

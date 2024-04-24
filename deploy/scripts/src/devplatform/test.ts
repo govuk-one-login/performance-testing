88891068e786de465dac5da985cae7e27e68c502
Abhinay Srivastava <117734822+abhinaysrivastavaperf@users.noreply.github.com>
2024-04-24 17:09:52 +0100

    Script added for testing in dev platform

---
+++ deploy/scripts/src/devplatform/test.ts
@@ -0,0 +1,64 @@
+import { iterationsStarted, iterationsCompleted } from '../common/utils/custom_metric/counter'
+import { type Options } from 'k6/options'
+import http, { type Response } from 'k6/http'
+import {
+  selectProfile,
+  type ProfileList,
+  describeProfile,
+  createScenario,
+  LoadProfile
+} from '../common/utils/config/load-profiles'
+import { timeGroup } from '../common/utils/request/timing'
+import { isStatusCode200, pageContentCheck } from '../common/utils/checks/assertions'
+import { getEnv } from '../common/utils/config/environment-variables'
+import { getThresholds } from '../common/utils/config/thresholds'
+
+const profiles: ProfileList = {
+  smoke: {
+    ...createScenario('frontEndScaling', LoadProfile.smoke)
+  },
+  lowVolume: {
+    ...createScenario('frontEndScaling', LoadProfile.full, 30)
+  },
+  load: {
+    ...createScenario('frontEndScaling', LoadProfile.full, 1000)
+  },
+  stress: {
+    ...createScenario('frontEndScaling', LoadProfile.full, 2000)
+  }
+}
+
+const loadProfile = selectProfile(profiles)
+const groupMap = {
+  frontEndScaling: [
+    'B01_FEScaling_01_CallDemoAPI'
+  ]
+} as const
+
+export const options: Options = {
+  scenarios: loadProfile.scenarios,
+  thresholds: getThresholds(groupMap),
+  tags: { name: '' }
+}
+
+export function setup(): void {
+  describeProfile(loadProfile)
+}
+
+const env = {
+  apiURL: getEnv('DEMO_NODE_ENDPOINT')
+}
+
+export function frontEndScaling(): void {
+  const groups = groupMap.frontEndScaling
+  let res: Response
+  iterationsStarted.add(1)
+
+  // B01_FEScaling_01_CallDemoAPI
+  res = timeGroup(groups[0], () => http.get(env.apiURL), {
+    isStatusCode200,
+    ...pageContentCheck('Welcome to the Platform SRE Show and tell')
+  })
+  iterationsCompleted.add(1)
+}
+

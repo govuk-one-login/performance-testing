ea630c32e96730822816cb27584eeb6031f40315
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-05-10 15:38:37 +0100

    Browser script test

--- deploy/scripts/build.mjs
+++ deploy/scripts/build.mjs
@@ -1,14 +1,14 @@
-import { build } from 'esbuild';
-import { copy } from 'esbuild-plugin-copy';
-import { sync } from 'glob';
-const outbase = './src';
-const outdir = './dist';
+import { build } from 'esbuild'
+import { copy } from 'esbuild-plugin-copy'
+import { sync } from 'glob'
+const outbase = './src'
+const outdir = './dist'
 
 build({
   entryPoints: sync('src/*/*.ts'),
   outbase,
   outdir,
-  target: 'es6',
+  target: 'es2017',
   format: 'esm',
   bundle: true,
   sourcemap: false,
@@ -24,12 +24,12 @@ build({
   ]
 })
   .then(() => {
-    console.log('Test scripts transpiled:');
+    console.log('Test scripts transpiled:')
     sync(outdir + '/*/*.js')
       .sort()
-      .forEach((file) => {
-        console.log(`+ \x1b[32m${file}\x1b[0m`);
-      });
+      .forEach(file => {
+        console.log(`+ \x1b[32m${file}\x1b[0m`)
+      })
   })
   // eslint-disable-next-line no-undef
-  .catch(() => process.exit(1));
+  .catch(() => process.exit(1))

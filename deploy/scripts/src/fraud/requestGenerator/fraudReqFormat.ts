a61d1fc8d5a0e625d8fe1573665cdcb500fbc55e
sachadorf13 <sacha.dorf@digital.cabinet-office.gov.uk>
2024-06-07 18:17:22 +0100

    Fraud script - Create and sign payload

---
+++ deploy/scripts/src/fraud/requestGenerator/fraudReqFormat.ts
@@ -0,0 +1,15 @@
+export interface FraudRequest {
+  iss: string
+  jti: string
+  iat: number
+  aud: string
+  events: {
+    'https://vocab.account.gov.uk/secevent/v1/notification/accountBlock': {
+      subject: {
+        format: string
+        uri: string
+      }
+      reason_admin: { en: string }
+    }
+  }
+}
---
+++ deploy/scripts/src/fraud/test.ts
@@ -0,0 +1,33 @@
+import { type Options } from 'k6/options'
+import {
+  selectProfile,
+  type ProfileList,
+  describeProfile,
+  createScenario,
+  LoadProfile
+} from '../common/utils/config/load-profiles'
+import { signRequest } from './requestGenerator/fraudReqGen'
+
+const profiles: ProfileList = {
+  smoke: {
+    ...createScenario('fraud', LoadProfile.smoke)
+  }
+}
+
+const loadProfile = selectProfile(profiles)
+
+export const options: Options = {
+  scenarios: loadProfile.scenarios,
+  thresholds: {
+    http_req_duration: ['p(95)<=1000', 'p(99)<=2500'], // 95th percentile response time <=1000ms, 99th percentile response time <=2500ms
+    http_req_failed: ['rate<0.05'] // Error rate <5%
+  }
+}
+
+export function setup(): void {
+  describeProfile(loadProfile)
+}
+
+export function fraud(): void {
+  console.log(signRequest())
+}

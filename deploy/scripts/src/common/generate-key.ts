5502d10b28cc13b2479b13a3559c118614bda667
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-06-20 19:01:48 +0100

    Add script for generating a key

--- deploy/scripts/src/common/generate-key.ts
+++ deploy/scripts/src/common/generate-key.ts
@@ -1,15 +1,42 @@
 import { type Options } from 'k6/options'
 import { createKey, signJwt } from './utils/authentication/jwt'
 import { crypto } from 'k6/experimental/webcrypto'
+import { ProfileList, selectProfile } from './utils/config/load-profiles'
+
+const profiles: ProfileList = {
+  smoke: {
+    hs256: {
+      vus: 1,
+      iterations: 1,
+      exec: 'hs256',
+      executor: 'per-vu-iterations'
+    },
+    es256: {
+      vus: 1,
+      iterations: 1,
+      exec: 'es256',
+      executor: 'per-vu-iterations'
+    }
+  }
+}
+const loadProfile = selectProfile(profiles)
 
 export const options: Options = {
-  vus: 1,
-  iterations: 1
+  scenarios: loadProfile.scenarios
 }
 
-export default async function (): Promise<void> {
+export async function hs256(): Promise<void> {
   const key = await createKey('HS256')
-  const x = await crypto.subtle.exportKey('jwk', key)
-  console.log(x)
-  console.log(await signJwt('HS256', key, {}))
+  const exportedKey = await crypto.subtle.exportKey('jwk', key)
+  console.log('Exported key:', exportedKey)
+  console.log(await signJwt('HS512', key, {}))
+}
+
+export async function es256(): Promise<void> {
+  const key = await createKey('ES256')
+  const publicKey = await crypto.subtle.exportKey('jwk', key.publicKey)
+  const privateKey = await crypto.subtle.exportKey('jwk', key.privateKey)
+  console.log('Public key:', publicKey)
+  console.log('Private key:', privateKey)
+  console.log(await signJwt('ES256', key.privateKey, {}))
 }

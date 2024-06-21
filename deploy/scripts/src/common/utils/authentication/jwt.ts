f8743b493172ff0917862110fa4b6c331b91cec2
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-06-21 09:48:40 +0100

    Update JWT JSDoc examples

--- deploy/scripts/src/common/utils/authentication/jwt.ts
+++ deploy/scripts/src/common/utils/authentication/jwt.ts
@@ -17,7 +17,7 @@ export type JwtHeader = {
   typ: 'JWT'
   alg: JwtAlgorithm
 }
-const algKeyMap: Record<JwtAlgorithm, HmacKeyGenParams | EcKeyGenParams> = {
+export const algKeyMap: Record<JwtAlgorithm, HmacKeyGenParams | EcKeyGenParams> = {
   HS256: { name: 'HMAC', hash: 'SHA-256' },
   HS384: { name: 'HMAC', hash: 'SHA-384' },
   HS512: { name: 'HMAC', hash: 'SHA-512' },
@@ -25,7 +25,7 @@ const algKeyMap: Record<JwtAlgorithm, HmacKeyGenParams | EcKeyGenParams> = {
   ES384: { name: 'ECDSA', namedCurve: 'P-384' },
   ES512: { name: 'ECDSA', namedCurve: 'P-521' }
 }
-const algParamMap: Record<JwtAlgorithm, 'HMAC' | Algorithm<'HMAC'> | EcdsaParams> = {
+export const algParamMap: Record<JwtAlgorithm, 'HMAC' | Algorithm<'HMAC'> | EcdsaParams> = {
   HS256: { name: 'HMAC' },
   HS384: { name: 'HMAC' },
   HS512: { name: 'HMAC' },
@@ -38,6 +38,11 @@ const algParamMap: Record<JwtAlgorithm, 'HMAC' | Algorithm<'HMAC'> | EcdsaParams
  * Create a key which can be used for signing JWTs
  * @param {JwtAlgorithm} type Algorithm to generate a key or key pair for
  * @returns {CryptoKey | CryptoKeyPair} Returns a crypto key or key pair depending on algorithm
+ * @example
+ * const hmacKey: CryptoKey = await createKey('HS256') // Key for HMAC
+ * const keys: CryptoKeyPair = await createKey('ES256') // Public/private key pair for ECDSA
+ * const publicKey = await crypto.subtle.exportKey('jwk', keys.publicKey)
+ * const privateKey = await crypto.subtle.exportKey('jwk', keys.privateKey)
  */
 export async function createKey(type: HmacAlgorithm): Promise<CryptoKey>
 export async function createKey(type: EcAlgorithm): Promise<CryptoKeyPair>
@@ -55,6 +60,14 @@ export async function createKey(type: JwtAlgorithm): Promise<CryptoKey | CryptoK
  * @param {CryptoKey} key CryptoKey to use for signing
  * @param {object} data Data payload to use in JWT
  * @returns {string} String representation of JWT
+ * @example
+ * // Import CryptoKey
+ * const jwk = JSON.parse(open('./example-data/hs256-key.json')) as JWK
+ * const key = await crypto.subtle.importKey('jwk', jwt, { name: 'ECDSA', namedCurve: 'P-256' }, true, ['sign', 'verify'])
+ * // Payload to sign
+ * const payload = { iss: 'joe', exp: 1300819380, 'http://example.com/is_root': true }
+ * // Create JWT string
+ * const jwt = await signJwt('EC256', keys.privateKey, payload)
  */
 export async function signJwt(type: JwtAlgorithm, key: CryptoKey, data: object): Promise<string> {
   const header = b64encode(JSON.stringify({ typ: 'JWT', alg: type }), 'rawurl')
@@ -70,6 +83,13 @@ export async function signJwt(type: JwtAlgorithm, key: CryptoKey, data: object):
  * @param {string} jwt JWT in string format
  * @param {CryptoKey} key CryptoKey used for verification
  * @returns {boolean} Boolean indicating whether the signature has been verified
+ * @example
+ * const keys: CryptoKeyPair = await createKey('ES256')
+ * const jwt = await signJwt('EC256', keys.privateKey, {'data':'value'})
+ * const ok: boolean = await verifyJwt(jwt, keys.publicKey)
+ * check(null, {
+ *  'jwt valid': () => ok
+ * })
  */
 export async function verifyJwt(jwt: string, key: CryptoKey): Promise<boolean> {
   const token = jwt.split('.')

d85b2dd29ea268ae7cfa58b52a633736510720bf
Abhinay Srivastava <117734822+abhinaysrivastavaperf@users.noreply.github.com>
2024-07-29 15:44:57 +0100

    Update ID Reuse scenario for dev validation

--- deploy/scripts/src/ipv-core/test.ts
+++ deploy/scripts/src/ipv-core/test.ts
@@ -739,7 +739,7 @@ export function idReuse(): void {
       () =>
         http.get(
           env.orchStubEndPoint +
-            `/authorize?journeyType=full&userIdText=${idReuseUserID.userID}&signInJourneyIdText=${signInJourneyId}&vtrText=P2&targetEnvironment=${environment}&reproveIdentity=NOT_PRESENT&emailAddress=${idReuseUserID.emailID}&votText=&jsonPayload=&evidenceJsonPayload=&error=recoverable`,
+            `/authorize?journeyType=full&userIdText=${idReuseUserID.userID}&signInJourneyIdText=${signInJourneyId}&vtrText=${env.vtrText}&targetEnvironment=${environment}&reproveIdentity=NOT_PRESENT&emailAddress=${idReuseUserID.emailID}&votText=&jsonPayload=&evidenceJsonPayload=&error=recoverable`,
           {
             headers: { Authorization: `Basic ${encodedCredentials}` },
             redirects: 0

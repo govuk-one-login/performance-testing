93206d00078c204ce67f8a1a30ab201d01f1b613
Thomas Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-09-05 14:32:12 +0100

    Fix retry mechanism

--- koa-stub/src/controllers/callback.controller.js
+++ koa-stub/src/controllers/callback.controller.js
@@ -81,23 +81,22 @@ const processCallback = async (ctx) => {
 };
 
 async function getUserInfo(ctx, access_token) {
-  let attempt = 0;
   let maxRetries = 3;
   console.log("Getting the userinfo request");
-  try {
-    let response = await ctx.oneLogin.userinfo(access_token);
-    console.log(response);
-    return response;
-  } catch (error) {
-    if (attempt < maxRetries) {
-      attempt += 1;
+  for (let attempt = 0; attempt < maxRetries; attempt++) {
+    try {
+      const response = await ctx.oneLogin.userinfo(access_token);
+      console.log(`Attempt ${attempt}: Response ${response}`);
+      return response;
+    } catch (error) {
+      console.warn(
+        `Attempt ${attempt}: Request to userinfo failed due to ${error}`
+      );
       const delay = 1000;
-      console.warn(`Request to userinfo failed due to ${error}`);
       await new Promise((resolve) => setTimeout(resolve, delay));
-      return await ctx.oneLogin.userinfo(access_token);
     }
-    throw new Error(`Userinfo endpoint not authorising`);
   }
+  throw new Error(`Userinfo endpoint not authorising`);
 }
 
 module.exports = {

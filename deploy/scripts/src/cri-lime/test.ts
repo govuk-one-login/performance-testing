8475f5d4e0054fe7c10d9a83f25ac60688ecdeb4
Richie Vinod <richie.vinod@digital.cabinet-office.gov.uk>
2025-01-20 10:29:39 +0000

    QA-714 Update Lime for L2 Spike tests

--- deploy/scripts/src/cri-lime/test.ts
+++ deploy/scripts/src/cri-lime/test.ts
@@ -93,8 +93,8 @@ const profiles: ProfileList = {
     ...createScenario('passport', LoadProfile.soak, 22, 8)
   },
   spikeNFR_L2: {
-    ...createScenario('fraud', LoadProfile.spikeNFRSignUp, 26, 8),
-    ...createScenario('passport', LoadProfile.spikeNFRSignUp, 22, 8)
+    ...createScenario('fraud', LoadProfile.spikeNFRSignUpL2, 26, 8),
+    ...createScenario('passport', LoadProfile.spikeNFRSignUpL2, 22, 8)
   },
   spikeSudden_L2: {
     ...createScenario('fraud', LoadProfile.spikeSudden, 27, 8),

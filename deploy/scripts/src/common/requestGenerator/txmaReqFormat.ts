ff7d2816e9ae44bd99f142911c8fb1adaff51b7a
Maddie Morris <maddie.morris@digital.cabinet-office.gov.uk>
2025-03-06 16:40:09 +0000

    adding TxMA Events to the ReqGen

--- deploy/scripts/src/common/requestGenerator/txmaReqFormat.ts
+++ deploy/scripts/src/common/requestGenerator/txmaReqFormat.ts
@@ -102,3 +102,240 @@ export interface DcmawAbortWeb {
     transaction_id: string
   }
 }
+
+export interface AuthAuthorizationInitiated {
+  client_id: string
+  component_id: string
+  event_name: string
+  event_timestamp_ms: number
+  extensions: {
+    'client-name': string
+    new_authentication_required: boolean
+  }
+  timestamp: number
+  user: {
+    govuk_signin_journey_id: string
+    ip_address: string
+    persistent_session_id: string
+    session_id: string
+  }
+}
+
+export interface AuthCodeVerified {
+  client_id: string
+  component_id: string
+  event_name: string
+  event_timestamp_ms: number
+  extension: {
+    MFACodeEntered: string
+    'account-recovery': boolean
+    'journey-type': string
+    loginFailureCount: number
+    'mfa-type': string
+    'notification-type': string
+  }
+  timestamp: number
+  user: {
+    email: string
+    govuk_sigin_journey_id: string
+    ip_address: string
+    persistent_session_id: string
+    session_id: string
+    user_id: string
+  }
+}
+
+export interface AuthUpdateProfilePhoneNumber {
+  client_id: string
+  component_id: string
+  event_name: string
+  event_timestamp_ms: number
+  extensions: {
+    'account-recovery': boolean
+    'mfa-type': string
+    phone_number_country_code: number
+  }
+  timestamp: number
+  user: {
+    email: string
+    govuk_signin_journey_id: string
+    ip_address: string
+    persistent_session_id: string
+    phone: string
+    session_id: string
+    user_id: string
+  }
+}
+
+export interface IPVJourneyStart {
+  client_id: string
+  component_id: string
+  event_name: string
+  event_timestamp_ms: number
+  extensions: {
+    reprove_identity: boolean
+    vtr: string
+  }
+  restricted: {
+    device_infomation: {
+      encoded: string
+    }
+  }
+  timestamp: number
+  user: {
+    govuk_signin_journey_id: string
+    ip_address: string
+    session_id: string
+    user_id: string
+  }
+}
+
+export interface IPVSubJourneyStart {
+  client_id: string
+  component_id: string
+  event_name: string
+  event_timestamp_ms: number
+  extensions: {
+    journey_type: string
+  }
+  restricted: {
+    device_information: {
+      encoded: string
+    }
+  }
+  timestamp: number
+  user: {
+    govuk_signin_journey_id: string
+    ip_address: string
+    session_id: string
+    user_id: string
+  }
+}
+
+export interface IPVDLCRIVCIssued {
+  client_id: string
+  component_id: string
+  event_name: string
+  event_timestamp_ms: number
+  extensions: {
+    evidence: {
+      activityHistoryScore: number
+      checkDetails: {
+        activityForm: string
+        checkMethod: string
+        identityCheckPolicy: string
+      }
+      ci: string
+      failedCheckDetails: {
+        checkMethod: string
+        identityCheckPolicy: string
+      }
+      strengthScore: number
+      txn: string
+      type: string
+      validityScore: number
+    }
+    iss: string
+  }
+  restricted: {
+    address: {
+      postalCode: string
+    }
+    birthDate: {
+      value: string
+    }
+    drivingPermit: {
+      expiryDate: string
+      issueDate: string
+      issueNumber: string
+      issuedBy: string
+      personalNumber: string
+    }
+    name: {
+      description: string
+      nameParts: {
+        type: string
+        validFrom: string
+        validUntil: string
+        value: string
+      }
+      validFrom: string
+      validUntil: string
+    }
+    timestamp: number
+    user: {
+      govuk_signin_journey_id: string
+      ip_address: string
+      persistent_session_id: string
+      session_id: string
+      user_id: string
+    }
+  }
+}
+
+export interface IPVAddressCRIVCIssued {
+  client_id: string
+  component_id: string
+  event_name: string
+  event_timestamp_ms: number
+  extensions: {
+    addressEntered: number
+    iss: string
+  }
+  restricted: {
+    address: {
+      addressCountry: string
+      addressLocality: string
+      buildingName: string
+      buildingNumber: string
+      postalCode: string
+      streetName: string
+      uprn: number
+      validFrom: string
+      validUntil: string
+    }
+  }
+  timestamp: number
+  user: {
+    govuk_signin_journey_id: string
+    ip_address: string
+    persistent_session_id: string
+    session_id: string
+    user_id: string
+  }
+}
+
+export interface IPVKBVCRIStart {
+  client_id: string
+  component_id: string
+  event_name: string
+  event_timestamp_ms: number
+  restricted: {
+    device_infomation: {
+      encoded: string
+    }
+  }
+  timestamp: number
+  user: {
+    govuk_signin_journey_id: string
+    ip_address: string
+    persistent_session_id: string
+    session_id: string
+    user_id: string
+  }
+}
+
+export interface IPVKBVCRIEnd {
+  client_id: string
+  component_id: string
+  event_name: string
+  event_timestamp_ms: number
+  timestamp: number
+  user: {
+    govuk_signin_journey_id: string
+    ip_address: string
+    persistent_session_id: string
+    session_id: string
+    user_id: string
+  }
+}
--- deploy/scripts/src/common/requestGenerator/txmaReqGen.ts
+++ deploy/scripts/src/common/requestGenerator/txmaReqGen.ts
@@ -1,5 +1,19 @@
-import { uuidv4 } from '../../common/utils/jslib/index'
-import { AuthLogInSuccess, AuthCreateAccount, AuthAuthorisationReqParsed, DcmawAbortWeb } from './txmaReqFormat'
+import { uuidv4 } from '../utils/jslib/index'
+import {
+  AuthLogInSuccess,
+  AuthCreateAccount,
+  AuthAuthorisationReqParsed,
+  DcmawAbortWeb,
+  AuthAuthorizationInitiated,
+  AuthCodeVerified,
+  AuthUpdateProfilePhoneNumber,
+  IPVJourneyStart,
+  IPVSubJourneyStart,
+  IPVDLCRIVCIssued,
+  IPVAddressCRIVCIssued,
+  IPVKBVCRIStart,
+  IPVKBVCRIEnd
+} from './txmaReqFormat'
 
 export function generateAuthCreateAccount(
   testID: string,
@@ -119,6 +133,265 @@ export function generateDcmawAbortWeb(userID: string, journeyID: string, emailID
       session_id: uuidv4(),
       persistent_session_id: uuidv4(),
       transaction_id: uuidv4()
+    }
+  }
+}
+
+export function generateAuthAuthorizationInitiated(journeyID: string): AuthAuthorizationInitiated {
+  return {
+    client_id: 'performanceTestClientId',
+    component_id: 'perfTest',
+    event_name: 'AUTH_AUTHORIZATION_INITIATED',
+    event_timestamp_ms: Math.floor(Date.now()),
+    extensions: {
+      'client-name': 'PerfTest',
+      new_authentication_required: false
+    },
+    timestamp: Math.floor(Date.now() / 1000),
+    user: {
+      govuk_signin_journey_id: journeyID,
+      ip_address: '1.2.3.4',
+      persistent_session_id: uuidv4(),
+      session_id: uuidv4()
+    }
+  }
+}
+
+export function generateAuthCodeVerified(emailID: string, journeyID: string, userID: string): AuthCodeVerified {
+  return {
+    client_id: 'performanceTestClientId',
+    component_id: 'perfTest',
+    event_name: 'AUTH_CODE_VERIFIED',
+    event_timestamp_ms: Math.floor(Date.now()),
+    extension: {
+      MFACodeEntered: '123',
+      'account-recovery': false,
+      'journey-type': 'SIGN_IN',
+      loginFailureCount: 0,
+      'mfa-type': 'SMS',
+      'notification-type': 'SMS'
+    },
+    timestamp: Math.floor(Date.now() / 1000),
+    user: {
+      email: emailID,
+      govuk_sigin_journey_id: journeyID,
+      ip_address: '1.2.3.4',
+      persistent_session_id: uuidv4(),
+      session_id: uuidv4(),
+      user_id: userID
+    }
+  }
+}
+
+export function generateAuthUpdatePhone(
+  emailID: string,
+  journeyID: string,
+  userID: string
+): AuthUpdateProfilePhoneNumber {
+  return {
+    client_id: 'performanceTestClientId',
+    component_id: 'perfTest',
+    event_name: 'AUTH_UPDATE_PROFILE_PHONE_NUMBER',
+    event_timestamp_ms: Math.floor(Date.now()),
+    extensions: {
+      'account-recovery': false,
+      'mfa-type': 'SMS',
+      phone_number_country_code: 44
+    },
+    timestamp: Math.floor(Date.now() / 1000),
+    user: {
+      email: emailID,
+      govuk_signin_journey_id: journeyID,
+      ip_address: '1.2.3.4',
+      persistent_session_id: uuidv4(),
+      phone: '07123456789',
+      session_id: uuidv4(),
+      user_id: userID
+    }
+  }
+}
+
+export function generateIPVJourneyStart(journeyID: string, userID: string): IPVJourneyStart {
+  return {
+    client_id: 'performanceTestClientId',
+    component_id: 'perfTest',
+    event_name: 'IPV_JOURNEY_START',
+    event_timestamp_ms: Math.floor(Date.now()),
+    extensions: {
+      reprove_identity: false,
+      vtr: 'CI'
+    },
+    restricted: {
+      device_infomation: {
+        encoded: 'RW5jb2RlZCBkYXRhIHdpbGwgYmUgaGVyZQ==' //pragma: allowlist secret
+      }
+    },
+    timestamp: Math.floor(Date.now() / 1000),
+    user: {
+      govuk_signin_journey_id: journeyID,
+      ip_address: '1.2.3.4',
+      session_id: uuidv4(),
+      user_id: userID
+    }
+  }
+}
+
+export function generateIPVSubJourneyStart(journeyID: string, userID: string): IPVSubJourneyStart {
+  return {
+    client_id: 'performanceTestClientId',
+    component_id: 'perfTest',
+    event_name: 'IPV_SUBJOURNEY_START',
+    event_timestamp_ms: Math.floor(Date.now()),
+    extensions: {
+      journey_type: 'REUSE_EXISTING_IDENTITY'
+    },
+    restricted: {
+      device_information: {
+        encoded: 'RW5jb2RlZCBkYXRhIHdpbGwgYmUgaGVyZQ==,' //pragma: allowlist secret
+      }
+    },
+    timestamp: Math.floor(Date.now() / 1000),
+    user: {
+      govuk_signin_journey_id: journeyID,
+      ip_address: '1.2.3.4',
+      session_id: uuidv4(),
+      user_id: userID
+    }
+  }
+}
+
+export function generateIPVDLCRIVCIssued(userID: string, journeyID: string): IPVDLCRIVCIssued {
+  return {
+    client_id: 'performanceTestClientId',
+    component_id: 'perfTest',
+    event_name: 'IPV_DL_CRI_VC_ISSUED',
+    event_timestamp_ms: Math.floor(Date.now()),
+    extensions: {
+      evidence: {
+        activityHistoryScore: 4,
+        checkDetails: {
+          activityForm: '20200101',
+          checkMethod: 'vpip',
+          identityCheckPolicy: 'policy'
+        },
+        ci: 'string', //This should be an array[string], i don't know how to implement that/if it needs changing.
+        failedCheckDetails: {
+          checkMethod: 'vpip',
+          identityCheckPolicy: 'policy'
+        },
+        strengthScore: 4,
+        txn: 'UNKOWN',
+        type: 'UNKNOWN',
+        validityScore: 4
+      },
+      iss: 'perfTest'
+    },
+    restricted: {
+      address: {
+        postalCode: 'AB12 3CD'
+      },
+      birthDate: {
+        value: '19900101'
+      },
+      drivingPermit: {
+        expiryDate: '20300101',
+        issueDate: '20200101',
+        issueNumber: '1234',
+        issuedBy: 'DVLA',
+        personalNumber: '12345'
+      },
+      name: {
+        description: 'name',
+        nameParts: {
+          type: 'FamilyName',
+          validFrom: '19900101',
+          validUntil: '20500101',
+          value: 'Smith'
+        },
+        validFrom: '19900101',
+        validUntil: '20500101'
+      },
+      timestamp: Math.floor(Date.now() / 1000),
+      user: {
+        govuk_signin_journey_id: journeyID,
+        ip_address: '1.2.3.4',
+        persistent_session_id: uuidv4(),
+        session_id: uuidv4(),
+        user_id: userID
+      }
+    }
+  }
+}
+
+export function generateIPVAddressCRIVCIssued(journeyID: string, userID: string): IPVAddressCRIVCIssued {
+  return {
+    client_id: 'performanceTestClientId',
+    component_id: 'perfTest',
+    event_name: 'IPV_ADDRESS_CRI_VC_ISSUED',
+    event_timestamp_ms: Math.floor(Date.now()),
+    extensions: {
+      addressEntered: 1,
+      iss: 'perfTest'
+    },
+    restricted: {
+      address: {
+        addressCountry: 'GB',
+        addressLocality: 'London',
+        buildingName: 'Highfield House',
+        buildingNumber: '44',
+        postalCode: 'AB12 3CD',
+        streetName: 'HIGH STREET',
+        uprn: 123456789012,
+        validFrom: '19900101',
+        validUntil: '20300101'
+      }
+    },
+    timestamp: Math.floor(Date.now() / 1000),
+    user: {
+      govuk_signin_journey_id: journeyID,
+      ip_address: '1.2.3.4',
+      persistent_session_id: uuidv4(),
+      session_id: uuidv4(),
+      user_id: userID
+    }
+  }
+}
+
+export function generateIPVKBVCRIStart(journeyID: string, userID: string): IPVKBVCRIStart {
+  return {
+    client_id: 'performanceTestClientID',
+    component_id: 'perfTest',
+    event_name: 'IPV_KBV_CRI_START',
+    event_timestamp_ms: Math.floor(Date.now()),
+    restricted: {
+      device_infomation: {
+        encoded: 'RW5jb2RlZCBkYXRhIHdpbGwgYmUgaGVyZQ==,' //pragma: allowlist secret
+      }
+    },
+    timestamp: Math.floor(Date.now() / 1000),
+    user: {
+      govuk_signin_journey_id: journeyID,
+      ip_address: '1.2.3.4',
+      persistent_session_id: uuidv4(),
+      session_id: uuidv4(),
+      user_id: userID
+    }
+  }
+}
+
+export function generateIPVKBVCRIEnd(journeyID: string, userID: string): IPVKBVCRIEnd {
+  return {
+    client_id: 'performanceTestClientID',
+    component_id: 'perfTest',
+    event_name: 'IPV_KBV_CRI_END',
+    event_timestamp_ms: Math.floor(Date.now()),
+    timestamp: Math.floor(Date.now() / 1000),
+    user: {
+      govuk_signin_journey_id: journeyID,
+      ip_address: '1.2.3.4',
+      persistent_session_id: uuidv4(),
+      session_id: uuidv4(),
+      user_id: userID
     }
   }
 }

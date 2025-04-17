f7c8cf4651f7c9c775528bf755059af6569d563b
Jamie Lutton <jamie.lutton@digital.cabinet-office.gov.uk>
2025-04-17 09:39:48 +0100

    DCMAW-12496: Add comment to crypto util

--- deploy/scripts/src/mobile/sts/utils/crypto.ts
+++ deploy/scripts/src/mobile/sts/utils/crypto.ts
@@ -1,6 +1,11 @@
 import { algParamMap, JwtAlgorithm } from '../../../common/utils/authentication/jwt'
 import { b64encode } from 'k6/encoding'
 
+// I'm aware that there are existing cryptographic functions in the common utils
+// However I noticed that the k6 crypto library is marked as deprecated (see https://grafana.com/docs/k6/latest/javascript-api/k6-experimental/webcrypto/),
+// so I implemented functions like signJwt, etc. specifically as utils for STS, which could eventually replace the common utils,
+// but I didn't want to risk breaking any existing performance test scripts
+
 export async function signJwt(
   type: JwtAlgorithm,
   key: CryptoKey,

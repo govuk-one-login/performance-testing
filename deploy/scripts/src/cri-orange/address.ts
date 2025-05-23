8386a8cdb51436037bf5e9fe7392a6face330fc0
“Narasimha <narasimha.yanamala@digital.cabinet-office.gov.uk>
2025-05-23 15:19:09 +0100

    Revert "[QA-1120]: I4 Spike profile for Address and  ME"
    This reverts commit aeec981e7f5c3d6e388312fbb8f29e10de7a91cc.

--- deploy/scripts/src/cri-orange/address.ts
+++ deploy/scripts/src/cri-orange/address.ts
@@ -163,10 +163,6 @@ const profiles: ProfileList = {
       ],
       exec: 'addressME'
     }
-  },
-  perf006Iteration4SpikeTest: {
-    ...createI3SpikeSignUpScenario('address', 100, 15, 101),
-    ...createI3SpikeSignUpScenario('addressME', 1030, 15, 1031)
   }
 }
 

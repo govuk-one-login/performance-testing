80eba7fd74a5793fa943b39ac77e7ee763eea319
Jamie Lutton <jamie.lutton@digital.cabinet-office.gov.uk>
2025-05-28 18:24:07 +0100

    Remove unused code

--- deploy/scripts/src/mobile/sts/testSteps/backend.ts
+++ deploy/scripts/src/mobile/sts/testSteps/backend.ts
@@ -159,36 +159,6 @@ export function exchangeAccessToken(groupName: string, accessToken: string, scop
   return res.json('access_token') as string
 }
 
-export function exchangePreAuthorizedCode(
-  groupName: string,
-  preAuthorizedCode: string,
-  accessToken: string,
-  clientId: string
-) {
-  timeGroup(
-    groupName,
-    () => {
-      return http.post(
-        `${config.stsBaseUrl}/token`,
-        {
-          grant_type: 'urn:ietf:params:oauth:grant-type:pre-authorized_code',
-          client_id: clientId,
-          'pre-authorized_code': preAuthorizedCode
-        },
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization: ` Bearer ${accessToken}`
-          }
-        }
-      )
-    },
-    {
-      isStatusCode200
-    }
-  )
-}
-
 export function simulateCallToStsJwks(groupName: string): void {
   timeGroup(
     groupName,

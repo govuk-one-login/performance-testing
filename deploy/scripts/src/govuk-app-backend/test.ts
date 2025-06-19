64cb9995ab3b543bb0cdb04fec648e40d0c00628
Abhinay Srivastava <117734822+abhinaysrivastavaperf@users.noreply.github.com>
2025-06-19 13:03:09 +0100

    Add loop for refresh token and limit revoke call

--- deploy/scripts/src/govuk-app-backend/test.ts
+++ deploy/scripts/src/govuk-app-backend/test.ts
@@ -124,22 +124,24 @@ export function govUkAppBackend(): void {
 
   const refreshToken = res.json('refresh_token') as string
 
-  // B01_GovUKAppBackend_04_TokenRefreshCall
-  timeGroup(
-    groups[3],
-    () =>
-      http.post(
-        env.tokenExchangeURL + '/dev/oauth2/token',
-        {
-          grant_type: 'refresh_token',
-          client_id: env.clientID,
-          refresh_token: refreshToken
-        },
-        tokenExchangeHeaders
-      ),
-    { isStatusCode200, ...pageContentCheck('expires_in') }
-  )
-  sleepBetween(0.5, 1)
+  for (let i = 0; i < 9; i++) {
+    // B01_GovUKAppBackend_04_TokenRefreshCall
+    timeGroup(
+      groups[3],
+      () =>
+        http.post(
+          env.tokenExchangeURL + '/dev/oauth2/token',
+          {
+            grant_type: 'refresh_token',
+            client_id: env.clientID,
+            refresh_token: refreshToken
+          },
+          tokenExchangeHeaders
+        ),
+      { isStatusCode200, ...pageContentCheck('expires_in') }
+    )
+    sleepBetween(0.5, 1)
+  }
 
   const revokeTokenHeaders = {
     headers: {
@@ -148,19 +150,21 @@ export function govUkAppBackend(): void {
     }
   }
 
-  // B01_GovUKAppBackend_05_RevokeToken
-  timeGroup(
-    groups[4],
-    () =>
-      http.post(
-        env.authURL + '/oauth2/revoke',
-        {
-          token: refreshToken
-        },
-        revokeTokenHeaders
-      ),
-    { isStatusCode200 }
-  )
+  if (Math.random() <= 0.67) {
+    // B01_GovUKAppBackend_05_RevokeToken
+    timeGroup(
+      groups[4],
+      () =>
+        http.post(
+          env.authURL + '/oauth2/revoke',
+          {
+            token: refreshToken
+          },
+          revokeTokenHeaders
+        ),
+      { isStatusCode200 }
+    )
+  }
 
   iterationsCompleted.add(1)
 }

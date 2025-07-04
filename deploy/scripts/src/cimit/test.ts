32791174e02c5d000f6371849498e46a5edd01da
vineeth-kunju <vineeth.kunju@digital.cabinet-office.gov.uk>
2025-07-04 14:51:36 +0100

    Updated according the review comments 0407

--- deploy/scripts/src/cimit/test.ts
+++ deploy/scripts/src/cimit/test.ts
@@ -9,6 +9,7 @@ import {
   createI3SpikeSignUpScenario
 } from '../common/utils/config/load-profiles'
 import http from 'k6/http'
+import { SharedArray } from 'k6/data'
 import { type Options } from 'k6/options'
 import { timeGroup } from '../common/utils/request/timing'
 import { isStatusCode200, pageContentCheck } from '../common/utils/checks/assertions'
@@ -33,12 +34,12 @@ const profiles: ProfileList = {
     ...createScenario('cimitSignUpAPIs', LoadProfile.full, 400, 5)
   },
   dataCreationGenerateCIs: {
-    generateCIs: {
+    cimitSignUpAPIs: {
       executor: 'per-vu-iterations',
       vus: 100,
       iterations: 500,
       maxDuration: '60m',
-      exec: 'generateCIs'
+      exec: 'cimitSignUpAPIs'
     }
   },
   perf006Iteration4PeakTest: {
@@ -55,11 +56,12 @@ const profiles: ProfileList = {
 
 const loadProfile = selectProfile(profiles)
 const groupMap = {
-  cimitAPIs: [
+  cimitSignUpAPIs: [
     'B01_CIMIT_01_PutContraIndicator',
     'B03_CIMIT_02_PostMitigations',
     'B02_CIMIT_03_GetContraIndicatorCredentials'
-  ]
+  ],
+  cimitSignInAPIs: ['B02_CIMIT_03_GetContraIndicatorCredentials']
 } as const
 
 export const options: Options = {
@@ -76,13 +78,28 @@ const env = {
   envURL: getEnv('IDENTITY_CIMIT_APIURL')
 }
 
+interface RetrieveSubjectId {
+  subjectId: string
+}
+
+const csvData: RetrieveSubjectId[] = new SharedArray('Retrieve SubjectId', function () {
+  return open('./data/getSubjectId.csv')
+    .split('\n')
+    .slice(1)
+    .map(subjectId => {
+      return {
+        subjectId
+      }
+    })
+})
+
 const keys = {
   passport: JSON.parse(getEnv('IDENTITY_CIMIT_PASSPORTKEY')) as JWK,
   drivingLicense: JSON.parse(getEnv('IDENTITY_CIMIT_DLKEY')) as JWK
 }
 
 export async function cimitSignUpAPIs(): Promise<void> {
-  const groups = groupMap.cimitAPIs
+  const groups = groupMap.cimitSignUpAPIs
   const subjectID = 'urn:fdc:gov.uk:2022:' + uuidv4()
   const payloads = {
     putContraIndicatorPayload: generatePassportPayloadCI(subjectID),
@@ -118,7 +135,7 @@ export async function cimitSignUpAPIs(): Promise<void> {
   sleep(5)
 
   // B03_CIMIT_02_PostMitigations
-  timeGroup(groups[2], () => http.post(env.envURL + '/v1/contra-indicators/mitigate', postMitigationReqBody, params), {
+  timeGroup(groups[1], () => http.post(env.envURL + '/v1/contra-indicators/mitigate', postMitigationReqBody, params), {
     isStatusCode200,
     ...pageContentCheck('success')
   })
@@ -127,15 +144,16 @@ export async function cimitSignUpAPIs(): Promise<void> {
   sleep(5)
 
   // B02_CIMIT_03_GetContraIndicatorCredentials
-  timeGroup(groups[1], () => http.get(env.envURL + `/v1/contra-indicators?user_id=${subjectID}`, params), {
+  timeGroup(groups[2], () => http.get(env.envURL + `/v1/contra-indicators?user_id=${subjectID}`, params), {
     isStatusCode200,
     ...pageContentCheck('vc')
   })
+  console.log(subjectID)
 }
 
 export async function cimitSignInAPI(): Promise<void> {
-  const groups = groupMap.cimitAPIs
-  const subjectID = 'urn:fdc:gov.uk:2022:' + uuidv4() //Temporary
+  const retrieveData = csvData[Math.floor(Math.random() * csvData.length)]
+  const groups = groupMap.cimitSignInAPIs
   const params = {
     headers: {
       'govuk-signin-journey-id': uuidv4(),
@@ -146,55 +164,9 @@ export async function cimitSignInAPI(): Promise<void> {
   iterationsStarted.add(1)
 
   // B02_CIMIT_03_GetContraIndicatorCredentials
-  timeGroup(groups[1], () => http.get(env.envURL + `/v1/contra-indicators?user_id=${subjectID}`, params), {
+  timeGroup(groups[0], () => http.get(env.envURL + `/v1/contra-indicators?user_id=${retrieveData.subjectId}`, params), {
     isStatusCode200,
     ...pageContentCheck('vc')
   })
   iterationsCompleted.add(1)
-}
-
-export async function generateCIs(): Promise<void> {
-  const groups = groupMap.cimitAPIs
-  const subjectID = 'urn:fdc:gov.uk:2022:' + uuidv4()
-  const payloads = {
-    putContraIndicatorPayload: generatePassportPayloadCI(subjectID),
-    postMitigationsPayload: generateDrivingLicensePayloadMitigation(subjectID)
-  }
-  const createJwt = async (key: JWK, payload: object): Promise<string> => {
-    const escdaParam: EcKeyImportParams = { name: 'ECDSA', namedCurve: 'P-256' }
-    const importedKey = await webcrypto.subtle.importKey('jwk', key, escdaParam, true, ['sign'])
-    return signJwt('ES256', importedKey, payload)
-  }
-  const putContraIndicatorJWT = await createJwt(keys.passport, payloads.putContraIndicatorPayload)
-  const putContraIndicatorReqBody = JSON.stringify({ signed_jwt: putContraIndicatorJWT })
-  const postMitigationsJWT = await createJwt(keys.drivingLicense, payloads.postMitigationsPayload)
-  const postMitigationReqBody = JSON.stringify({ signed_jwts: [postMitigationsJWT] })
-  const params = {
-    headers: {
-      'govuk-signin-journey-id': uuidv4(),
-      'ip-address': '1.2.3.4'
-    }
-  }
-
-  iterationsStarted.add(1)
-  // B01_CIMIT_01_PutContraIndicator
-  timeGroup(
-    groups[0],
-    () => http.post(env.envURL + '/v1/contra-indicators/detect', putContraIndicatorReqBody, params),
-    {
-      isStatusCode200,
-      ...pageContentCheck('success')
-    }
-  )
-
-  sleep(5)
-
-  // B03_CIMIT_02_PostMitigations
-  timeGroup(groups[2], () => http.post(env.envURL + '/v1/contra-indicators/mitigate', postMitigationReqBody, params), {
-    isStatusCode200,
-    ...pageContentCheck('success')
-  })
-  iterationsCompleted.add(1)
-
-  console.log(subjectID)
 }

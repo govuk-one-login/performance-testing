aad7cda6bcd61427d4532bc94ba026db11a61bce
“Narasimha <narasimha.yanamala@digital.cabinet-office.gov.uk>
2025-12-17 09:59:31 +0000

    QA-1486 add Peak Test Load profile for Address

--- deploy/scripts/src/cri-orange/address.ts
+++ deploy/scripts/src/cri-orange/address.ts
@@ -206,6 +206,23 @@ const profiles: ProfileList = {
       ],
       exec: 'addressME'
     }
+  },
+  perf006Iteration8PeakTest: {
+    ...createI4PeakTestSignUpScenario('address', 100, 15, 101),
+    ...createI4PeakTestSignUpScenario('internationalAddress', 2, 12, 3),
+    addressME: {
+      executor: 'ramping-arrival-rate',
+      startRate: 1,
+      timeUnit: '10s',
+      preAllocatedVUs: 22,
+      maxVUs: 43,
+      stages: [
+        { target: 0, duration: '101s' }, // Wait until the happy path scenario ramps up to target load
+        { target: 70, duration: '71s' }, // Ramp up to target load
+        { target: 70, duration: '30m' } // Maintain a steady state at the target load for 30 minutes.
+      ],
+      exec: 'addressME'
+    }
   }
 }
 

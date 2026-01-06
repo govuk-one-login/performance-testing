f5301a976eac8ea24dd6eca18e4c3e7042728cbf
“Umair <umair.farooqui@digital.cabinet-office.gov.uk>
2026-01-06 16:17:16 +0000

    STS:Update profiles for I8 Comb Peak test

--- deploy/scripts/src/mobile/sts.ts
+++ deploy/scripts/src/mobile/sts.ts
@@ -137,6 +137,12 @@ const profiles: ProfileList = {
     ...createI4PeakTestSignUpScenario('authentication', 170, 27, 171),
     ...createI4PeakTestSignInScenario('reauthentication', 16, 27, 8),
     ...createI4PeakTestSignInScenario('walletCredentialIssuance', 38, 39, 18)
+  },
+  perf006Iteration8PeakTest: {
+    ...createI4PeakTestSignUpScenario('authentication', 160, 36, 161),
+    ...createI4PeakTestSignInScenario('reauthentication', 8, 27, 4),
+    ...createI4PeakTestSignInScenario('walletCredentialIssuance', 38, 39, 18),
+    ...createI4PeakTestSignInScenario('accountIntervention', 20, 15, 10)
   }
 }
 

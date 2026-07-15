0abc26d25b03ef4cf1f872abf477dc921be2a3c5
Abhinay Srivastava <abhinay.srivastava@digital.cabinet-office.gov.uk>
2026-07-15 16:32:19 +0100

    ESM conversion - App.js

--- koa-stub/src/app.js
+++ koa-stub/src/app.js
@@ -1,18 +1,16 @@
-const Koa = require("koa");
+import Koa from "koa";
+import cors from "@koa/cors";
+import router from "./router.js";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
 const app = new Koa();
-const cors = require("@koa/cors");
-const router = require("./router");
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 
 let config = {};
 if (process.env.AWS_SAM_LOCAL) {
-  config = {
-    endpoint: "http://host.docker.internal:8000",
-  };
+  config = { endpoint: "http://host.docker.internal:8000" };
 }
 
 app.context.ddbClient = new DynamoDBClient(config);
-
 app.use(cors()).use(router.routes());
 
-module.exports = app;
+export default app;

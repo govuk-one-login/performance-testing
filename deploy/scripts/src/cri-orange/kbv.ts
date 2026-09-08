2a3d743614ee09b9e80d619d3c6eefa836bd5826
Marianne Olabode <marianne.olabode@dsit.gov.uk>
2026-09-08 15:43:08 +0100

    Use new I10spike test function

--- deploy/scripts/src/cri-orange/kbv.ts
+++ deploy/scripts/src/cri-orange/kbv.ts
@@ -9,7 +9,8 @@ import {
   LoadProfile,
   createI3SpikeSignUpScenario,
   createI4PeakTestSignUpScenario,
-  createStressTestSignUpScenario
+  createStressTestSignUpScenario,
+  createSpikeTestSignUpScenario
 } from '../common/utils/config/load-profiles'
 import { env, encodedCredentials } from './utils/config'
 import { timeGroup } from '../common/utils/request/timing'
@@ -113,7 +114,7 @@ const profiles: ProfileList = {
     ...createStressTestSignUpScenario('kbv', 63, 12, 64)
   },
   perf006Iteration10SpikeTest: {
-    ...createI3SpikeSignUpScenario('kbv', 96, 12, 97)
+    ...createSpikeTestSignUpScenario('kbv', 96, 12, 97)
   }
 }
 
